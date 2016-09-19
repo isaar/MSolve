a4fceb80024b1257bfeedf75f46e987e2eb4a93b
Dimitris Tsapetis <dimtsap@hotmail.com>
2016-09-19 11:32:38 +0300

    Added SortAscending3() to TestVector class

--- MatricesTests/TestVector.cs
+++ MatricesTests/TestVector.cs
@@ -1,23 +1,24 @@
 ﻿using ISAAR.MSolve.Matrices;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+
 
 namespace MatricesTests
 {
-    [TestClass]
+    [TestFixture]
     class TestVector
     {
-        [TestMethod]
+        [Test]
         public void SortAscendingTest1()
         {
             Vector<double> vector1 = new Vector<double>(3);
             vector1[0] = 7;
-            vector1[0] = 8;
-            vector1[0] = 9;
+            vector1[1] = 8;
+            vector1[2] = 9;
             vector1.SortAscending();
 
             Vector<double> expectedVector = new Vector<double>(3);
@@ -25,19 +26,19 @@ namespace MatricesTests
             expectedVector[1] = 8;
             expectedVector[2] = 9;
 
-            Assert.AreEqual(vector1[0], expectedVector[0]);
-            Assert.AreEqual(vector1[1], expectedVector[1]);
-            Assert.AreEqual(vector1[2], expectedVector[2]);
+            Assert.AreEqual(expectedVector[0], vector1[0]);
+            Assert.AreEqual(expectedVector[1], vector1[1]);
+            Assert.AreEqual(expectedVector[2], vector1[2]);
 
         }
 
-        [TestMethod]
+        [Test]
         public void SortAscendingTest2()
         {
             Vector<double> vector2 = new Vector<double>(3);
             vector2[0] = 8;
-            vector2[0] = 9;
-            vector2[0] = 7;
+            vector2[1] = 9;
+            vector2[2] = 7;
             vector2.SortAscending();
 
             Vector<double> expectedVector = new Vector<double>(3);
@@ -45,10 +46,32 @@ namespace MatricesTests
             expectedVector[1] = 8;
             expectedVector[2] = 9;
 
-            Assert.AreEqual(vector2[0], expectedVector[0]);
-            Assert.AreEqual(vector2[1], expectedVector[1]);
-            Assert.AreEqual(vector2[2], expectedVector[2]);
+            Assert.AreEqual(expectedVector[0], vector2[0]);
+            Assert.AreEqual(expectedVector[1], vector2[1]);
+            Assert.AreEqual(expectedVector[2], vector2[2]);
 
+        }
+
+        [Test]
+        public void SortAscendingTest3()
+        {
+            Vector<double> vector3 = new Vector<double>(4);
+            vector3[0] = 8;
+            vector3[1] = 9;
+            vector3[2] = 7;
+            vector3[3] = 9;
+            vector3.SortAscending();
+
+            Vector<double> expectedVector = new Vector<double>(4);
+            expectedVector[0] = 7;
+            expectedVector[1] = 8;
+            expectedVector[2] = 9;
+            expectedVector[3] = 9;
+
+            Assert.AreEqual(expectedVector[0], vector3[0]);
+            Assert.AreEqual(expectedVector[1], vector3[1]);
+            Assert.AreEqual(expectedVector[2], vector3[2]);
+            Assert.AreEqual(expectedVector[3], vector3[3]);
         }
 
     }
