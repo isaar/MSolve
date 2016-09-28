5a28512c561b56030587da4c89126f4facb1691e
Dimitris Tsapetis <dimtsap@hotmail.com>
2016-09-28 21:59:13 +0300

    Implemented IGAModelReader

--- ISSAR.MSolve.IGAPreProcessor/IGAModel.cs
+++ ISSAR.MSolve.IGAPreProcessor/IGAModel.cs
@@ -9,18 +9,127 @@ namespace ISSAR.MSolve.IGAPreProcessor
 {
     public class IGAModel
     {
+        private int totalDofs;
         private int numberOfDimensions;
+
+        private int numberOfCPKsi;
+        private int numberOfCPHeta;
+        private int numberOfCPZeta;
+        //private int thickness;
+
         private int degreeKsi;
         private int degreeHeta;
         private int degreeZeta;
         private Vector<double> knotValueVectorKsi;
         private Vector<double> knotValueVectorHeta;
         private Vector<double> knotValueVectorZeta;
-        private int totalDofs;
-        private IList<ControlPoint> controlPoints = new List<ControlPoint>();
-        private IList<Knot> knots = new List<Knot>();
-        private IList<Element> elements = new List<Element>();
+        private readonly IList<ControlPoint> controlPoints = new List<ControlPoint>();
+        private readonly IList<Knot> knots = new List<Knot>();
+        private readonly IList<Element> elements = new List<Element>();
+        private readonly IList<IGALoad> loads = new List<IGALoad>();
 
+        #region Properties
+        public int TotalDOFs
+        {
+            get { return this.totalDofs; }
+            set { numberOfDimensions=value; }
+        }
 
+        public int NumberOfDimensions
+        {
+            get { return this.numberOfDimensions; }
+            set { numberOfDimensions = value; }
+        }
+
+        public int DegreeKsi
+        {
+            get { return this.degreeKsi; }
+            set { degreeKsi = value; }
+        }
+
+        public int DegreeHeta
+        {
+            get { return this.degreeHeta; }
+            set { degreeHeta = value; }
+        }
+
+        public int DegreeZeta
+        {
+            get { return this.degreeZeta; }
+            set { degreeZeta = value; }
+        }
+
+        public Vector<double> KnotValueVectorKsi
+        {
+            get { return this.knotValueVectorKsi; }
+            set { knotValueVectorKsi = value; }
+        }
+
+        public Vector<double> KnotValueVectorHeta
+        {
+            get { return this.knotValueVectorHeta; }
+            set { knotValueVectorHeta = value; }
+        }
+
+        public Vector<double> KnotValueVectorZeta
+        {
+            get { return this.knotValueVectorZeta; }
+            set { knotValueVectorZeta = value; }
+        }
+
+        public int NumberOfCPKsi
+        {
+            get { return numberOfCPKsi; }
+            set { numberOfCPKsi = value; }
+        }
+
+        public int NumberOfCPHeta
+        {
+            get { return numberOfCPHeta; }
+            set { numberOfCPHeta = value; }
+        }
+
+        public int NumberOfCPZeta
+        {
+            get { return numberOfCPZeta; }
+            set { numberOfCPZeta = value; }
+        }
+
+        public IList<ControlPoint> ControlPoints
+        {
+            get { return controlPoints; }
+        }
+
+        public IList<Knot> Knots
+        {
+            get { return knots; }
+        }
+
+        public IList<Element> Elements
+        {
+            get { return elements; }
+        }
+
+        public IList<IGALoad> Loads
+        {
+            get { return loads; }
+        }
+
+        #endregion
+
+        #region Data Creation Routines
+        public void CreateDataStructures()
+        {
+
+        }
+        #endregion
+
+        public void Clear()
+        {
+            this.controlPoints.Clear();
+            this.knots.Clear();
+            this.elements.Clear();
+
+        }
     }
 }
--- ISSAR.MSolve.IGAPreProcessor/Readers/IGAModelReader.cs
+++ ISSAR.MSolve.IGAPreProcessor/Readers/IGAModelReader.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ISAAR.MSolve.Matrices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,148 @@ using System.Threading.Tasks;
 
 namespace ISSAR.MSolve.IGAPreProcessor.Readers
 {
+
+    
+
     public static class IGAModelReader
     {
-        
-        public static void CreateModelFromFile(IGAModel Model, string fileName)
+        enum Attributes
         {
-
+            numberofdimensions,
+            degreeksi, degreeheta, degreezeta,
+            numberofcpksi, numberofcpheta, numberofcpzeta,
+            knotvaluevectorksi, knotvaluevectorheta, knotvaluevectorzeta,
+            cpcoord, end, thickness
         }
+
+
+        public static void CreateModelFromFile(IGAModel model, string fileName)
+        {
+            char[] delimeters = { ' ', '=' };
+
+            String[] text = System.IO.File.ReadAllLines(fileName);
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                String[] line = text[i].Split(delimeters);
+                Attributes name;
+                if (line[i] == null)
+                {
+                    i++;
+                    continue;
+                }
+                try
+                {
+                    name = (Attributes)Enum.Parse(typeof(Attributes), line[0]);
+                }catch (Exception e)
+                {
+                    throw new KeyNotFoundException("Variable name is not found " + line[0]);
+                }
+
+                switch (name)
+                {
+                    case Attributes.numberofdimensions:
+                        model.NumberOfDimensions = Int32.Parse(line[1]);
+                        break;
+                    case Attributes.degreeksi:
+                        model.DegreeKsi = Int32.Parse(line[1]);
+                        break;
+                    case Attributes.degreeheta:
+                        model.DegreeHeta = Int32.Parse(line[1]);
+                        break;
+                    case Attributes.degreezeta:
+                        model.DegreeZeta = Int32.Parse(line[1]);
+                        break;
+                    case Attributes.numberofcpksi:
+                        model.NumberOfCPKsi= Int32.Parse(line[1]);
+                        break;
+                    case Attributes.numberofcpheta:
+                        model.NumberOfCPHeta = Int32.Parse(line[1]);
+                        break;
+                    case Attributes.numberofcpzeta:
+                        model.NumberOfCPZeta = Int32.Parse(line[1]);
+                        break;
+                    case Attributes.knotvaluevectorksi:
+                        if (model.DegreeKsi==0 || model.NumberOfCPKsi == 0)
+                        {
+                            throw new ArgumentOutOfRangeException("Degree Ksi and number of Control Points per axis Ksi must be defined before Knot Value Vector Ksi.");
+                        }
+                        model.KnotValueVectorKsi = new Vector<double>(model.NumberOfCPKsi + model.DegreeKsi + 1);
+                        for (int j = 0; j < model.NumberOfCPKsi + model.DegreeKsi + 1; j++)
+                        {
+                            model.KnotValueVectorKsi[j] = Double.Parse(line[j + 1]);
+                        }
+                        break;
+                    case Attributes.knotvaluevectorheta:
+                        if (model.DegreeHeta == 0 || model.NumberOfCPHeta == 0)
+                        {
+                            throw new ArgumentOutOfRangeException("Degree Heta and number of Control Points per axis Heta must be defined before Knot Value Vector Heta.");
+                        }
+                        model.KnotValueVectorHeta = new Vector<double>(model.NumberOfCPHeta + model.DegreeHeta + 1);
+                        for (int j = 0; j < model.NumberOfCPHeta + model.DegreeHeta + 1; j++)
+                        {
+                            model.KnotValueVectorHeta[j] = Double.Parse(line[j + 1]);
+                        }
+                        break;
+                    case Attributes.knotvaluevectorzeta:
+                        if (model.DegreeZeta == 0 || model.NumberOfCPZeta == 0)
+                        {
+                            throw new ArgumentOutOfRangeException("Degree Zeta and number of Control Points per axis Zeta must be defined before Knot Value Vector Zeta.");
+                        }
+                        model.KnotValueVectorZeta = new Vector<double>(model.NumberOfCPZeta + model.DegreeZeta + 1);
+                        for (int j = 0; j < model.NumberOfCPZeta + model.DegreeZeta + 1; j++)
+                        {
+                            model.KnotValueVectorZeta[j] = Double.Parse(line[j + 1]);
+                        }
+                        break;
+                    case Attributes.cpcoord:
+                        if (model.NumberOfDimensions == 2)
+                        {
+                            Matrix2D<double> cpCoordinates = new Matrix2D<double>(model.NumberOfCPKsi*model.NumberOfCPHeta,3);
+                            cpCoordinates[0, 0] = Double.Parse(line[1]);
+                            cpCoordinates[0, 1] = Double.Parse(line[2]);
+                            cpCoordinates[0, 2] = Double.Parse(line[3]);
+                            for (int j=1; j< model.NumberOfCPKsi * model.NumberOfCPHeta; j++)
+                            {
+                                i++;
+                                line = text[i].Split(delimeters);
+                                cpCoordinates[j, 0] = Double.Parse(line[0]);
+                                cpCoordinates[j, 1] = Double.Parse(line[1]);
+                                cpCoordinates[j, 2] = Double.Parse(line[2]);
+                            }
+                            // model.CreateModelData();
+                        }
+                        else
+                        {
+                            Matrix2D<double> cpCoordinates = new Matrix2D<double>(model.NumberOfCPKsi * model.NumberOfCPHeta* model.NumberOfCPZeta, 4);
+                            cpCoordinates[0, 0] = Double.Parse(line[1]);
+                            cpCoordinates[0, 1] = Double.Parse(line[2]);
+                            cpCoordinates[0, 2] = Double.Parse(line[3]);
+                            cpCoordinates[0, 3] = Double.Parse(line[4]);
+                            for (int j = 1; j < model.NumberOfCPKsi * model.NumberOfCPHeta* model.NumberOfCPZeta; j++)
+                            {
+                                i++;
+                                line = text[i].Split(delimeters);
+                                cpCoordinates[j, 0] = Double.Parse(line[0]);
+                                cpCoordinates[j, 1] = Double.Parse(line[1]);
+                                cpCoordinates[j, 2] = Double.Parse(line[2]);
+                                cpCoordinates[j, 2] = Double.Parse(line[3]);
+                            }
+                            // model.CreateModelData();
+                        }
+                        break;
+                    case Attributes.end:
+                        return;
+                }
+                    
+
+            }
+        }
+
+
+
+
+
+
     }
 }
