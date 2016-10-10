681ad6bc5a595780a3d83b61dde37e08ad7edeb7
Dimitris Tsapetis <dimtsap@hotmail.com>
2016-10-10 16:06:31 +0300

    Added Integration folder with unimplemented classes BSPLines1D, NURBS2D, NURBS3D

---
+++ ISSAR.MSolve.IGAPreProcessor/Integration/ShapeNURBS3D.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ISSAR.MSolve.IGAPreProcessor.Integration
+{
+    class ShapeNURBS3D
+    {
+    }
+}
