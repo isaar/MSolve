d6d0b152f9ccd732afba144ce62c116f7b3f6f5f
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2017-01-28 20:43:29 +0200

    Gauss integration up to order 10.
    Signed-off-by: Serafeim Bakalakos <serafeim.bakalakos@outlook.com>

--- ISAAR.MSolve.XFEM/Integration/GaussPoints/IntegrationRules2D.cs
+++ ISAAR.MSolve.XFEM/Integration/GaussPoints/IntegrationRules2D.cs
@@ -14,6 +14,12 @@ namespace ISAAR.MSolve.XFEM.Integration.GaussPoints
         public static readonly IntegrationRule2D Order2x2 = new IntegrationRule2D(Order2, Order2);
         public static readonly IntegrationRule2D Order3x3 = new IntegrationRule2D(Order3, Order3);
         public static readonly IntegrationRule2D Order4x4 = new IntegrationRule2D(Order4, Order4);
+        public static readonly IntegrationRule2D Order5x5 = new IntegrationRule2D(Order5, Order5);
+        public static readonly IntegrationRule2D Order6x6 = new IntegrationRule2D(Order6, Order6);
+        public static readonly IntegrationRule2D Order7x7 = new IntegrationRule2D(Order7, Order7);
+        public static readonly IntegrationRule2D Order8x8 = new IntegrationRule2D(Order8, Order8);
+        public static readonly IntegrationRule2D Order9x9 = new IntegrationRule2D(Order9, Order9);
+        public static readonly IntegrationRule2D Order10x10 = new IntegrationRule2D(Order10, Order10);
 
         public IReadOnlyList<GaussPoint2D> Points { get; }
 
