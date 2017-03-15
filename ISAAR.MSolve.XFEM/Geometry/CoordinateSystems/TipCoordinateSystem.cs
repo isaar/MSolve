d2f3d7731a0a7b9e94bc2c37978613dcc74f665a
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2017-03-15 08:19:57 +0200

    Renamed some things in the local system of the tip enrichment and switched around the signs. It doesn't change anything but makes sense now.
    Signed-off-by: Serafeim Bakalakos <serafeim.bakalakos@outlook.com>

--- ISAAR.MSolve.XFEM/Geometry/CoordinateSystems/TipCoordinateSystem.cs
+++ ISAAR.MSolve.XFEM/Geometry/CoordinateSystems/TipCoordinateSystem.cs
@@ -14,24 +14,24 @@ namespace ISAAR.MSolve.XFEM.Geometry.CoordinateSystems
         public readonly double cosa;
         public readonly double sina;
 
-        // The local coordinates of the tip.
-        private readonly double x1Tip;
-        private readonly double x2Tip;
+        // The local coordinates of the global origin
+        private readonly double x1Origin;
+        private readonly double x2Origin;
 
         public TipCoordinateSystem(ICartesianPoint2D tipCoordinates, double orientation)
         {
             cosa = Math.Cos(orientation);
             sina = Math.Sin(orientation);
-            x1Tip = cosa * tipCoordinates.X + sina * tipCoordinates.Y;
-            x2Tip = -sina * tipCoordinates.X + cosa * tipCoordinates.Y;
+            x1Origin = - cosa * tipCoordinates.X - sina * tipCoordinates.Y;
+            x2Origin = sina * tipCoordinates.X - cosa * tipCoordinates.Y;
         }
 
         public PolarPoint2D TransformCartesianGlobalToPolarLocal(ICartesianPoint2D cartesianGlobalPoint)
         {
             double globX = cartesianGlobalPoint.X;
             double globY = cartesianGlobalPoint.Y;
-            double x1 = cosa * globX + sina * globY - x1Tip;
-            double x2 = -sina * globX + cosa * globY - x2Tip;
+            double x1 = cosa * globX + sina * globY + x1Origin;
+            double x2 = -sina * globX + cosa * globY + x2Origin;
             double r = Math.Sqrt(x1 * x1 + x2 * x2);
             double theta = Math.Atan2(x2, x1);
             return new PolarPoint2D(r, theta);
