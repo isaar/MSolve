369ac1ab8eea33175baf1706464aa355e5c10f8c
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2017-04-18 04:37:02 +0300

    Table for associating row, column pairs with values
    Signed-off-by: Serafeim Bakalakos <serafeim.bakalakos@outlook.com>

---
+++ ISAAR.MSolve.XFEM/Utilities/ITable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISAAR.MSolve.XFEM.Utilities
+{
+    interface ITable<TRow, TColumn, TValue>: IEnumerable<Tuple<TRow, TColumn, TValue>>
+    {
+        int EntryCount { get; }
+        TValue this[TRow row, TColumn col] { get; }
+        bool Contains(TRow row, TColumn col);
+        IEnumerable<TValue> GetValuesOfRow(TRow row);
+    }
+}
---
+++ ISAAR.MSolve.XFEM/Utilities/Table.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISAAR.MSolve.XFEM.Utilities
+{
+    // TODO: Add slicing features
+    class Table<TRow, TColumn, TValue> : ITable<TRow, TColumn, TValue>
+    {
+        protected readonly Dictionary<TRow, Dictionary<TColumn, TValue>> data;
+
+        public Table()
+        {
+            this.data = new Dictionary<TRow, Dictionary<TColumn, TValue>>();
+        }
+
+        public int EntryCount
+        {
+            get
+            {
+                int count = 0;
+                foreach (var wholeRow in data)
+                {
+                    count += wholeRow.Value.Count;
+                }
+                return count;
+            }
+        }
+
+        public TValue this[TRow row, TColumn col]
+        {
+            get { return data[row][col]; }
+            set
+            {
+                Dictionary<TColumn, TValue> wholeRow;
+                bool rowExists = data.TryGetValue(row, out wholeRow);
+                if (!rowExists)
+                {
+                    wholeRow = new Dictionary<TColumn, TValue>();
+                    data.Add(row, wholeRow);
+                }
+                if (wholeRow.ContainsKey(col))
+                {
+                    throw new ArgumentException("The entry (row, column) = (" + row + ", "
+                        + col + ") already exists.");
+                }
+                else wholeRow.Add(col, value);
+            }
+        }
+
+        public bool Contains(TRow row, TColumn col)
+        {
+            Dictionary<TColumn, TValue> wholeRow;
+            bool containsRow = data.TryGetValue(row, out wholeRow);
+            if (!containsRow) return false; 
+            else return wholeRow.ContainsKey(col);
+        }
+
+        public IEnumerator<Tuple<TRow, TColumn, TValue>> GetEnumerator()
+        {
+            foreach (var wholeRow in data)
+            {
+                foreach (var colValPair in wholeRow.Value)
+                {
+                    yield return new Tuple<TRow, TColumn, TValue>(wholeRow.Key, colValPair.Key, colValPair.Value);
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerable<TValue> GetValuesOfRow(TRow row)
+        {
+            return data[row].Values;
+        }
+    }
+}
