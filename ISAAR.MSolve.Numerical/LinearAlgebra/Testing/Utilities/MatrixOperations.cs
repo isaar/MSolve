bb778cb24bcb81506a648f0b72489a6d3743dfab
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2018-02-16 13:55:08 +0200

    Added IntelMKL bindings. Had to increase .NET version to 4.6.1.
    Signed-off-by: Serafeim Bakalakos <serafeim.bakalakos@outlook.com>

---
+++ ISAAR.MSolve.Numerical/LinearAlgebra/Testing/Utilities/MatrixOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISAAR.MSolve.Numerical.LinearAlgebra.Testing.Utilities
+{
+    public static class MatrixOperations
+    {
+        public static double[] AddArrays(double[] a, double[] b)
+        {
+            if (a.Length != b.Length) throw new RankException("Cannot add arrays with different length");
+            double[] c = new double[a.Length];
+            for (int i = 0; i < a.Length; ++i)
+            {
+                c[i] = a[i] + b[i];
+            }
+            return c;
+        }
+
+        public static double DotMultiplyArrays(double[] a, double[] b)
+        {
+            if (a.Length != b.Length) throw new RankException("Cannot add arrays with different length");
+            double sum = 0.0;
+            for (int i = 0; i < a.Length; ++i)
+            {
+                sum += a[i] * b[i];
+            }
+            return sum;
+        }
+    }
+}
