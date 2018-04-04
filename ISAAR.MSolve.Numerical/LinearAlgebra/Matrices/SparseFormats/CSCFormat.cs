5b560cb7b01de728c29379fe703cc6a3ae7ce5a0
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2018-04-04 20:31:28 +0300

    Sparse matrix format containers for safely passing around values and index arrays .
    Signed-off-by: Serafeim Bakalakos <serafeim.bakalakos@outlook.com>

---
+++ ISAAR.MSolve.Numerical/LinearAlgebra/Matrices/SparseFormats/CSCFormat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISAAR.MSolve.Numerical.LinearAlgebra.Matrices.SparseFormats
+{
+    public class CSCFormat : ISparseFormat
+    {
+        public CSCFormat(double[] values, int[] rowIndices, int[] columnOffsets)
+        {
+            RawValuesArray = values;
+            RawIndexArrays = new Dictionary<string, IReadOnlyList<int>>(2)
+            {
+                { "Row indices", rowIndices },
+                { "Column offsets", columnOffsets }
+            };
+        }
+
+        public IReadOnlyList<double> RawValuesArray { get; }
+        public Dictionary<string, IReadOnlyList<int>> RawIndexArrays { get; }
+    }
+}
---
+++ ISAAR.MSolve.Numerical/LinearAlgebra/Matrices/SparseFormats/ISparseFormat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISAAR.MSolve.Numerical.LinearAlgebra.Matrices.SparseFormats
+{
+    public interface ISparseFormat
+    {
+        IReadOnlyList<double> RawValuesArray { get; }
+        Dictionary<string, IReadOnlyList<int>> RawIndexArrays { get; }
+    }
+}
