5a38f63304259ef39b7d50912c4afa93dc4ff48f
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2018-04-18 18:43:38 +0300

    Extension method for reordering all matrices into dense ones.

--- ISAAR.MSolve.LinearAlgebra/Matrices/MatrixExtensions.cs
+++ ISAAR.MSolve.LinearAlgebra/Matrices/MatrixExtensions.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ISAAR.MSolve.LinearAlgebra.Exceptions;
 using ISAAR.MSolve.LinearAlgebra.Output;
 using ISAAR.MSolve.LinearAlgebra.Testing.Utilities;
 
@@ -30,6 +31,38 @@ namespace ISAAR.MSolve.LinearAlgebra.Matrices
         public static void AddIntoThis(this IMatrix matrix1, IMatrixView matrix2)
         {
             matrix1.AxpyIntoThis(matrix2, 1.0);
+        }
+
+        public static Matrix Reorder(this IMatrixView matrix, IReadOnlyList<int> permutation, bool oldToNew)
+        {
+            int order = matrix.NumRows;
+            if (matrix.NumColumns != order)
+            {
+                throw new NonMatchingDimensionsException("This operation works on square matrices only.");
+            }
+            if (permutation.Count != order)
+            {
+                throw new NonMatchingDimensionsException($"This matrix has order = {order}, while the permutation vector"
+                    + $" has {permutation.Count} entries.");
+            }
+            var reordered = Matrix.CreateZero(order, order);
+            if (oldToNew)
+            {
+                for (int j = 0; j < order; ++j)
+                {
+                    int newCol = permutation[j];
+                    for (int i = 0; i < order; ++i) reordered[permutation[i], newCol] = matrix[i, j];
+                }
+            }
+            else
+            {
+                for (int j = 0; j < order; ++j)
+                {
+                    int oldCol = permutation[j];
+                    for (int i = 0; i < order; ++i) reordered[i, j] = matrix[permutation[i], oldCol];
+                }
+            }
+            return reordered;
         }
 
         public static IMatrixView Scale(this IMatrixView matrix, double scalar)
