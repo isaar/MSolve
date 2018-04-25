154bd5004608fd5d5311038f43525f45e2a6b769
George Soimiris <gsoimoiris@gmail.com>
2018-04-25 21:37:52 +0300

    changes in implicit integration provider

--- ISAAR.MSolve.Analyzers/Interfaces/IImplicitIntegrationProvider.cs
+++ ISAAR.MSolve.Analyzers/Interfaces/IImplicitIntegrationProvider.cs
@@ -1,5 +1,6 @@
 ﻿using ISAAR.MSolve.Numerical.LinearAlgebra.Interfaces;
 using ISAAR.MSolve.Solvers.Interfaces;
+using System.Collections.Generic;
 
 namespace ISAAR.MSolve.Analyzers.Interfaces
 {
@@ -7,6 +8,8 @@ namespace ISAAR.MSolve.Analyzers.Interfaces
     {
         void CalculateEffectiveMatrix(ILinearSystem subdomain, ImplicitIntegrationCoefficients coefficients);
         void ProcessRHS(ILinearSystem subdomain, ImplicitIntegrationCoefficients coefficients);
+        IDictionary<int, double[]> GetAccelerationsOfTimeStep(int timeStep);
+        IDictionary<int, double[]> GetVelocitiesOfTimeStep(int timeStep);
         void GetRHSFromHistoryLoad(int timeStep);
         void MassMatrixVectorProduct(ILinearSystem subdomain, IVector vIn, double[] vOut);
         void DampingMatrixVectorProduct(ILinearSystem subdomain, IVector vIn, double[] vOut);
