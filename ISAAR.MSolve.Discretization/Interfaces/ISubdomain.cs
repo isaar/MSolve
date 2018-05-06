324609be468c8ee6ed710dc0af44c8559a673f25
Dimitris Tsapetis <dimtsap@hotmail.com>
2018-05-07 00:14:34 +0300

    Added ISubdomain interface to abstract Subdomain and Patch

---
+++ ISAAR.MSolve.Discretization/Interfaces/ISubdomain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ISAAR.MSolve.Numerical.LinearAlgebra.Interfaces;
+
+namespace ISAAR.MSolve.Discretization.Interfaces
+{
+    public interface ISubdomain
+    {
+		int ID { get; set; }
+		int TotalDOFs { get; set; }
+		bool MaterialsModified { get; set; }
+
+		Dictionary<int, IElement> ElementsDictionary { get; }
+		Dictionary<int, INode> NodesDictionary { get; }
+		Dictionary<int, Dictionary<DOFType, int>> NodalDOFsDictionary { get; }
+		Dictionary<int, Dictionary<DOFType, int>> GlobalNodalDOFsDictionary { get; }
+
+		IList<INode> Nodes { get; }
+		double[] Forces { get; }
+
+		void EnumerateDOFs();
+		void AssignGlobalNodalDOFsFromModel();
+		void BuildNodesDictionary();
+	    double[] GetLocalVectorFromGlobal(IElement element, IVector globalVector);
+    }
+}
