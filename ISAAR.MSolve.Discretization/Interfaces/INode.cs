477eed985bc4bd544c51d8078140b05806124e3a
Dimitris Tsapetis <dimtsap@hotmail.com>
2018-05-07 00:12:58 +0300

    Added INode interface to abstract Nodes and ControlPoints

---
+++ ISAAR.MSolve.Discretization/Interfaces/INode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ISAAR.MSolve.Discretization.Interfaces
+{
+    public interface INode
+    {
+		int ID { get; set; }
+		double X { get; set; }
+		double Y { get; set; }
+		double Z { get; set; }
+
+		List<DOFType> Constraints { get; }
+		Dictionary<int, IElement> ElementsDictionary { get; }
+		Dictionary<int, ISubdomain> SubdomainsDictionary { get; }
+
+		string ToString();
+		void BuildSubdomainDictionary();
+	}
+}
