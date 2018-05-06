722ef3d283969d0c895ac5cb978ca536e87dcd0e
Dimitris Tsapetis <dimtsap@hotmail.com>
2018-05-07 00:13:25 +0300

    Added IElement interface to abstract Finite and IGA Element

---
+++ ISAAR.MSolve.Discretization/Interfaces/IElement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ISAAR.MSolve.Discretization.Interfaces
+{
+    public interface IElement
+    {
+		int ID { get; set; }
+		ISubdomain Subdomain { get; set; }
+		int[] DOFs { get; }
+		IList<INode> Nodes { get; }
+	    Dictionary<int, INode> NodesDictionary { get; }
+		IElementType ElementType { get; set; }
+	}
+}
