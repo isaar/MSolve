e685cebb98ab69755f5616c110381a089f52c446
Maria Tavlaki <mariataylaki@gmail.com>
2018-05-07 04:48:54 +0300

    Implement NonLinearSubdomainUpdater, SubdomainGlobalMapping.

---
+++ ISAAR.MSolve.Analyzers/NonLinearSubdomainUpdater.cs
@@ -0,0 +1,49 @@
+﻿using ISAAR.MSolve.Analyzers.Interfaces;
+using ISAAR.MSolve.FEM.Entities;
+using ISAAR.MSolve.Numerical.LinearAlgebra;
+using ISAAR.MSolve.Numerical.LinearAlgebra.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ISAAR.MSolve.Analyzers
+{
+    public class NonLinearSubdomainUpdater : INonLinearSubdomainUpdater
+    {
+        private readonly Subdomain subdomain;
+
+        public NonLinearSubdomainUpdater(Subdomain subdomain)
+        {
+            this.subdomain = subdomain;
+        }
+
+        public IVector GetRHSFromSolution(IVector solution, IVector dSolution) //TODO leave 
+        {
+            var forces = new Vector(this.subdomain.TotalDOFs);
+            foreach (Element element in this.subdomain.ElementsDictionary.Values)
+            {
+                //var localSolution = GetLocalVectorFromGlobal(element, solution);//TODOMaria: This is where the element displacements are calculated //removeMaria
+                //var localdSolution = GetLocalVectorFromGlobal(element, dSolution);//removeMaria
+                double[] localSolution = this.subdomain.CalculateElementNodalDisplacements(element, solution);
+                double[] localdSolution = this.subdomain.CalculateElementNodalDisplacements(element, dSolution);
+                element.ElementType.CalculateStresses(element, localSolution, localdSolution);
+                if (element.ElementType.MaterialModified)
+                    element.Subdomain.MaterialsModified = true;
+                double[] f = element.ElementType.CalculateForces(element, localSolution, localdSolution);
+                this.subdomain.AddLocalVectorToGlobal(element, f, forces.Data);
+            }
+            return forces;
+        }
+
+        public void ResetState()
+        {
+            foreach (Element element in this.subdomain.ElementsDictionary.Values) element.ElementType.ClearMaterialStresses();
+        }
+
+        public void UpdateState()
+        {
+            foreach (Element element in this.subdomain.ElementsDictionary.Values) element.ElementType.SaveMaterialState();
+        }
+    }
+}
---
+++ ISAAR.MSolve.Analyzers/SubdomainGlobalMapping.cs
@@ -0,0 +1,50 @@
+﻿using ISAAR.MSolve.Analyzers.Interfaces;
+using ISAAR.MSolve.FEM.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ISAAR.MSolve.Analyzers
+{
+    public class SubdomainGlobalMapping : ISubdomainGlobalMapping
+    {
+        private readonly Subdomain subdomain;
+        public SubdomainGlobalMapping(Subdomain subdomain)
+        {
+            this.subdomain = subdomain;
+        }
+
+        public void SplitGlobalVectorToSubdomain(double[] vIn, double[] vOut)
+        {
+            foreach (int nodeID in this.subdomain.GlobalNodalDOFsDictionary.Keys)
+            {
+                Dictionary<DOFType, int> dofTypes = this.subdomain.NodalDOFsDictionary[nodeID];
+                foreach (DOFType dofType in dofTypes.Keys)
+                {
+                    int localDOF = this.subdomain.NodalDOFsDictionary[nodeID][dofType];
+                    int globalDOF = this.subdomain.GlobalNodalDOFsDictionary[nodeID][dofType];
+                    if (localDOF > -1 && globalDOF > -1) vOut[localDOF] = vIn[globalDOF];
+                }
+            }
+        }
+
+        public void SubdomainToGlobalVector(double[] vIn, double[] vOut)
+        {
+            foreach (int nodeID in this.subdomain.GlobalNodalDOFsDictionary.Keys)
+            {
+                Dictionary<DOFType, int> dofTypes = this.subdomain.NodalDOFsDictionary[nodeID];
+                foreach (DOFType dofType in dofTypes.Keys)
+                {
+                    int localDOF = this.subdomain.NodalDOFsDictionary[nodeID][dofType];
+                    int globalDOF = this.subdomain.GlobalNodalDOFsDictionary[nodeID][dofType];
+                    if (localDOF > -1 && globalDOF > -1) vOut[globalDOF] += vIn[localDOF];
+                }
+            }
+        }
+
+        public void SubdomainToGlobalVectorMeanValue(double[] vIn, double[] vOut)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
