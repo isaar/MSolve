359122a542f098315eea8c65cdd8c6339358f896
Dimitris Tsapetis <dimtsap@hotmail.com>
2018-05-12 14:11:24 +0300

    Modifies IElementType  to keep the minmum needed changes for Problem Structural

--- ISAAR.MSolve.Discretization/Interfaces/IElementType.cs
+++ ISAAR.MSolve.Discretization/Interfaces/IElementType.cs
@@ -15,20 +15,9 @@ namespace ISAAR.MSolve.Discretization.Interfaces
 
 	public interface IElementType
     {
-	    int ID { get; }
-	    ElementDimensions ElementDimensions { get; }
 		IElementDOFEnumerator DOFEnumerator { get; set; }
-		IList<IList<DOFType>> GetElementDOFTypes(IElement element);
-	    bool MaterialModified { get; }
 	    IMatrix2D StiffnessMatrix(IElement element);
 	    IMatrix2D MassMatrix(IElement element);
 	    IMatrix2D DampingMatrix(IElement element);
-	    void ResetMaterialModified();
-	    Tuple<double[], double[]> CalculateStresses(IElement element, double[] localDisplacements, double[] localdDisplacements);
-	    double[] CalculateForces(IElement element, double[] localDisplacements, double[] localdDisplacements);
-	    double[] CalculateForcesForLogging(IElement element, double[] localDisplacements);
-	    void SaveMaterialState();
-	    void ClearMaterialState();
-	    void ClearMaterialStresses();
 	}
 }
