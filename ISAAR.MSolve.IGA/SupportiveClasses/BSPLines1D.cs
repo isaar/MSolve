bc05d582b0ac39b5dc6a8b29e44cba5ecbdc0f84
Dimitris Tsapetis <dimtsap@hotmail.com>
2018-05-13 14:28:35 +0300

    Added SecondDerivatives in BSPLine1D

--- ISAAR.MSolve.IGA/SupportiveClasses/BSPLines1D.cs
+++ ISAAR.MSolve.IGA/SupportiveClasses/BSPLines1D.cs
@@ -20,6 +20,8 @@ namespace ISAAR.MSolve.IGA.Problems.SupportiveClasses
 
         public double[,] BSPLineDerivativeValues { get; private set; }
 
+		public double [,] BSPLineSecondDerivativeValues { get; private set; }
+
         public BSPLines1D(int degree, IVector knotValueVector, IVector parametricCoordinates)
         {
             if (degree <= 0)
@@ -38,23 +40,15 @@ namespace ISAAR.MSolve.IGA.Problems.SupportiveClasses
             int numberOfGaussPoints = ParametricCoordinates.Length;
             BSPLineValues = new double[numberOfControlPoints + Degree, numberOfGaussPoints];
             BSPLineDerivativeValues = new double[numberOfControlPoints + Degree, numberOfGaussPoints];
+			BSPLineSecondDerivativeValues = new double[numberOfControlPoints + Degree, numberOfGaussPoints];
 
-            // BSPLineBasisFunctions.Degree=0
             for (int i = 0; i < numberOfGaussPoints; i++)
                 for (int j = 0; j < numberOfControlPoints+Degree; j++)
-                    if (KnotValueVector[j]<=ParametricCoordinates[i]&& ParametricCoordinates[i] < KnotValueVector[j + 1])
-                    {
-                        BSPLineValues[j, i] = 1;
-                    }else
-                    {
-                        BSPLineValues[j, i] = 0;
-                    }
+	                if (KnotValueVector[j]<=ParametricCoordinates[i]&& ParametricCoordinates[i] < KnotValueVector[j + 1])
+		                BSPLineValues[j, i] = 1;
+	                else
+		                BSPLineValues[j, i] = 0;
 
-
-                    //BSPLineValues[j,i] = (KnotValueVector[j] <= ParametricCoordinates[i] &&
-                    //    ParametricCoordinates[i] < KnotValueVector[j + 1]) ? 1 : 0;
-
-            //BSPLinesBasisFunctions.Degree1:p
             for (int i = 1; i <= Degree; i++)
             {
                 for (int j = 0; j < numberOfControlPoints+Degree-i; j++)
@@ -65,7 +59,9 @@ namespace ISAAR.MSolve.IGA.Problems.SupportiveClasses
                         double additive2 = 0;
                         double additiveDerivative1 = 0;
                         double additiveDerivative2 = 0;
-                        double denominator1 = KnotValueVector[j + i] - KnotValueVector[j];
+	                    double additiveSecondDerivative1 = 0;
+	                    double additiveSecondDerivative2 = 0;
+						double denominator1 = KnotValueVector[j + i] - KnotValueVector[j];
                         double denominator2 = KnotValueVector[j + i + 1] - KnotValueVector[j + 1];
                         if (denominator1 != 0)
                         {
@@ -73,6 +69,7 @@ namespace ISAAR.MSolve.IGA.Problems.SupportiveClasses
                                 / denominator1 * BSPLineValues[j, k];
                             additiveDerivative1 = ((ParametricCoordinates[k] - KnotValueVector[j])
                                 * BSPLineDerivativeValues[j, k] + BSPLineValues[j, k]) / denominator1;
+	                        additiveSecondDerivative1 = Degree / denominator1 * BSPLineDerivativeValues[j, k];
                         }
                         if (denominator2 != 0)
                         {
@@ -80,9 +77,11 @@ namespace ISAAR.MSolve.IGA.Problems.SupportiveClasses
                                 / denominator2 * BSPLineValues[j + 1, k];
                             additiveDerivative2 = ((KnotValueVector[j + i + 1] - ParametricCoordinates[k])
                                 * BSPLineDerivativeValues[j + 1, k] - BSPLineValues[j + 1, k]) / denominator2;
+	                        additiveSecondDerivative2 = -Degree / denominator2 * BSPLineDerivativeValues[j + 1, k];
                         }
                         BSPLineValues[j, k] = additive1 + additive2;
                         BSPLineDerivativeValues[j, k] = additiveDerivative1 + additiveDerivative2;
+	                    BSPLineSecondDerivativeValues[j, k] = additiveSecondDerivative1 + additiveSecondDerivative2;
                     }
                 }
             }
