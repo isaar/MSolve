9eb1841ef757f855ba7d6815cce0b4a881bc7987
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2018-05-27 22:28:54 +0300

    Added proper timing for PCG solver.
    Signed-off-by: Serafeim Bakalakos <serafeim.bakalakos@outlook.com>

--- ISAAR.MSolve.XFEM/Solvers/PCGSolver.cs
+++ ISAAR.MSolve.XFEM/Solvers/PCGSolver.cs
@@ -18,20 +18,39 @@ using ISAAR.MSolve.XFEM.FreedomDegrees.Ordering;
 
 namespace ISAAR.MSolve.XFEM.Solvers
 {
-    class PCGSolver: SolverBase
+    class PCGSolver: ISolver
     {
+        private readonly Model2D model;
         private readonly double maxIterationsOverOrder; //roughly
         private readonly double tolerance;
+        private int iteration;
 
-        public PCGSolver(Model2D model, double maxIterationsOverOrder, double tolerance) : base(model)
+        public PCGSolver(Model2D model, double maxIterationsOverOrder, double tolerance)
         {
+            this.model = model;
             this.maxIterationsOverOrder = maxIterationsOverOrder;
             this.tolerance = tolerance;
+            Logger = new SolverLogger();
+
+        }
+        public IDofOrderer DofOrderer { get; protected set; }
+
+        public SolverLogger Logger { get; }
+
+        public Vector Solution { get; protected set; }
+
+        public void Initialize()
+        {
+            iteration = 0;
         }
 
-        public override void Solve()
+        public void Solve()
         {
-            // Interleaced and separate dof enumerators seem to have similar performance.
+            ++iteration;
+            var watch = new Stopwatch();
+            watch.Start();
+
+            // Interleaved and separate dof enumerators seem to have similar performance.
             DofOrderer = InterleavedDofOrderer.Create(model);
             //DofOrderer = DofOrdererSeparate.Create(model);
 
@@ -41,19 +60,26 @@ namespace ISAAR.MSolve.XFEM.Solvers
             //    "Stiffness matrix corresponding to free-free dofs is not symmetric");
             Vector rhs = CalcEffectiveRhs(Kuc);
 
+            watch.Stop();
+            Logger.LogDuration(iteration, "Linear system assembly", watch.ElapsedMilliseconds);
 
-            int maxIterations = (int)Math.Ceiling(Kuu.NumColumns * maxIterationsOverOrder);
-
-            // PCG
-            var pcg = new PreconditionedConjugateGradient(maxIterations, tolerance);
+            // Preconditioner
+            watch.Restart();
             (double[] diagonal, int firstZeroIdx) = Kuu.GetDiagonalAsArray(); // Preconditioner could be abstracted, but I think it depends on the solver.
             var preconditioner = new JacobiPreconditioner(diagonal);
             //var preconditioner = new IdentityPreconditioner(true);
-            (Vector x, IterativeStatistics statistics) = pcg.Solve(Kuu.BuildCSRMatrix(true), rhs, preconditioner);
+            watch.Stop();
+            Logger.LogDuration(iteration, "Building the preconditioner", watch.ElapsedMilliseconds);
 
-            // CG
+            // PCG
+            watch.Restart();
+            int maxIterations = (int)Math.Ceiling(Kuu.NumColumns * maxIterationsOverOrder);
+            var pcg = new PreconditionedConjugateGradient(maxIterations, tolerance);
+            (Vector x, IterativeStatistics statistics) = pcg.Solve(Kuu.BuildCSRMatrix(true), rhs, preconditioner);
             //var cg = new ConjugateGradient(maxIterations, tolerance);
             //(Vector x, IterativeStatistics statistics) = cg.Solve(Kuu.BuildCSRMatrix(true), rhs);
+            watch.Stop();
+            Logger.LogDuration(iteration, "PCG", watch.ElapsedMilliseconds);
 
             Console.WriteLine(statistics);
             Solution = x;
@@ -61,6 +87,24 @@ namespace ISAAR.MSolve.XFEM.Solvers
             #region Debugging
             //CheckPCG(model, DofOrderer, Kuu, Solution);
             #endregion
+        }
+
+        /// <summary>
+        /// The extended linear system is:
+        /// [Kcc Kcu; Kuc Kuu] * [uc; uu] = [Fc; Fu]
+        /// where c are the standard constrained dofs, f are the standard free dofs, e are the enriched dofs and 
+        /// u = Union(f,c) are both the dofs with unknown left hand side vectors: uu = [uf; ue].
+        /// To solve the system (for the unknowns ul):
+        /// i) Kuu * uu = Fu - Kuc * uc = Feff
+        /// ii) uu = Kuu \ Feff 
+        /// </summary>
+        /// <returns></returns>
+        private Vector CalcEffectiveRhs(DOKRowMajor globalUnconstrainedConstrained)
+        {
+            Vector Fu = model.CalculateFreeForces(DofOrderer);
+            Vector uc = model.CalculateConstrainedDisplacements(DofOrderer);
+            Vector Feff = Fu - globalUnconstrainedConstrained.MultiplyRight(uc);
+            return Feff;
         }
 
         private static void CheckPCG(Model2D model, IDofOrderer dofOrderer, DOKRowMajor Kuu, Vector solution)
