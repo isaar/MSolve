cbaec8b8b1d8e520a63b1763db2d6e9362a9a4f6
George Soimoiris <gsoimoiris@gmail.com>
2018-05-28 13:57:42 +0300

    added Hexa8NonLinear Example Test and MaterialD_v2

--- ISAAR.MSolve.Tests/Hexa8NonLinearNewtonRaphsonTest.cs
+++ ISAAR.MSolve.Tests/Hexa8NonLinearNewtonRaphsonTest.cs
@@ -4,6 +4,7 @@ using ISAAR.MSolve.FEM.Elements;
 using ISAAR.MSolve.FEM.Elements.SupportiveClasses;
 using ISAAR.MSolve.FEM.Entities;
 using ISAAR.MSolve.FEM.Materials;
+using ISAAR.MSolve.Materials;
 using ISAAR.MSolve.Numerical.LinearAlgebra;
 using ISAAR.MSolve.Problems;
 using ISAAR.MSolve.Solvers.Interfaces;
@@ -15,68 +16,21 @@ using Xunit;
 
 namespace ISAAR.MSolve.Tests
 {
-    public class Beam3DQuaternionNonlinearTest
+    public class Hexa8NonLinearNewtonRaphsonTest
     {
         [Fact]
-        public void SolveNLBeam()
-        {
-            var m = new Model();
-            m.NodesDictionary.Add(1, new Node() { ID = 1, X = 0, Y = 0, Z = 0 });
-            m.NodesDictionary.Add(2, new Node() { ID = 2, X = 5, Y = 0, Z = 0 });
-            m.NodesDictionary[1].Constraints.AddRange(new[] { DOFType.X, DOFType.Y, DOFType.Z, DOFType.RotX, DOFType.RotY, DOFType.RotZ });
-            m.ElementsDictionary.Add(1, new Element()
-            {
-                ID = 1,
-                ElementType = new Beam3DCorotationalQuaternion(m.Nodes, new ElasticMaterial3D() { YoungModulus = 2.1e6, PoissonRatio = 0.2 }, 1,
-                new BeamSection3D(0.06, 0.0002, 0.00045, 0.000818, 0.05, 0.05))
-            });
-            m.ElementsDictionary[1].AddNodes(m.Nodes);
-            m.SubdomainsDictionary.Add(1, new Subdomain() { ID = 1 });
-            m.SubdomainsDictionary[1].ElementsDictionary.Add(1, m.ElementsDictionary[1]);
-            m.Loads.Add(new Load() { Node = m.NodesDictionary[2], Amount = 100, DOF = DOFType.Y });
-
-            m.ConnectDataStructures();
-            VectorExtensions.AssignTotalAffinityCount();
-
-            var linearSystems = new Dictionary<int, ILinearSystem>();
-            linearSystems[1] = new SkylineLinearSystem(1, m.Subdomains[0].Forces);
-            var linearSystemsArray = new[] { linearSystems[1] };
-            var subdomainUpdaters = new[] { new NonLinearSubdomainUpdater(m.Subdomains[0]) };
-            var subdomainMappers = new[] { new SubdomainGlobalMapping(m.Subdomains[0]) };
-            int increments = 10;
-            int totalDOFs = m.TotalDOFs;
-            SolverSkyline solver = new SolverSkyline(linearSystems[1]);
-            ProblemStructural provider = new ProblemStructural(m, linearSystems);
-            NewtonRaphsonNonLinearAnalyzer childAnalyzer = new NewtonRaphsonNonLinearAnalyzer(solver, linearSystemsArray, subdomainUpdaters, subdomainMappers,
-                provider, increments, totalDOFs);
-
-            // Choose parent analyzer -> Parent: Static
-            StaticAnalyzer parentAnalyzer = new StaticAnalyzer(provider, childAnalyzer, linearSystems);
-            parentAnalyzer.BuildMatrices();
-            parentAnalyzer.Initialize();
-            parentAnalyzer.Solve();
-        }
-
-
-        [Fact]
-        public void TestBeam3DQuaternionNonlinearExample()
+        public void TestHexa8NonLinearNewtonRaphsonExample()
         {
             VectorExtensions.AssignTotalAffinityCount();
             double youngModulus = 21000.0;
             double poissonRatio = 0.3;
             double nodalLoad = 20000.0;
-            double area = 91.04;
-            double inertiaY = 2843.0;
-            double inertiaZ = 8091.0;
-            double torsionalInertia = 76.57;
-            double effectiveAreaY = 91.04;
-            double effectiveAreaZ = 91.04;
-            int nNodes = 3;
-            int nElems = 2;
-            int monitorNode = 3;
+            int nNodes = 12;
+            int nElems = 5;
+            int monitorNode = 12;
 
             // Create new 3D material
-            ElasticMaterial3D material = new ElasticMaterial3D
+            ElasticMaterial3D_v2 material = new ElasticMaterial3D_v2
             {
                 YoungModulus = youngModulus,
                 PoissonRatio = poissonRatio,
@@ -84,14 +38,32 @@ namespace ISAAR.MSolve.Tests
 
             // Node creation
             IList<Node> nodes = new List<Node>();
-            Node node1 = new Node { ID = 1, X = 0.0,   Y = 0.0, Z = 0.0 };
+            Node node1 = new Node { ID = 1, X = 0.0, Y = 0.0, Z = 0.0 };
             Node node2 = new Node { ID = 2, X = 100.0, Y = 0.0, Z = 0.0 };
             Node node3 = new Node { ID = 3, X = 200.0, Y = 0.0, Z = 0.0 };
-            
+            Node node4 = new Node { ID = 4, X = 300.0, Y = 0.0, Z = 0.0 };
+            Node node5 = new Node { ID = 5, X = 400.0, Y = 0.0, Z = 0.0 };
+            Node node6 = new Node { ID = 6, X = 500.0, Y = 0.0, Z = 0.0 };
+            Node node7 = new Node { ID = 7, X = 0.0, Y = 100.0, Z = 0.0 };
+            Node node8 = new Node { ID = 8, X = 100.0, Y = 100.0, Z = 0.0 };
+            Node node9 = new Node { ID = 9, X = 200.0, Y = 100.0, Z = 0.0 };
+            Node node10 = new Node { ID = 10, X = 300.0, Y = 100.0, Z = 0.0 };
+            Node node11 = new Node { ID = 11, X = 400.0, Y = 100.0, Z = 0.0 };
+            Node node12 = new Node { ID = 12, X = 500.0, Y = 100.0, Z = 0.0 };
+
             nodes.Add(node1);
             nodes.Add(node2);
             nodes.Add(node3);
-            
+            nodes.Add(node4);
+            nodes.Add(node5);
+            nodes.Add(node6);
+            nodes.Add(node7);
+            nodes.Add(node8);
+            nodes.Add(node9);
+            nodes.Add(node10);
+            nodes.Add(node11);
+            nodes.Add(node12);
+
             // Model creation
             Model model = new Model();
 
@@ -110,7 +82,7 @@ namespace ISAAR.MSolve.Tests
             model.NodesDictionary[1].Constraints.Add(DOFType.Z);
             model.NodesDictionary[1].Constraints.Add(DOFType.RotX);
             model.NodesDictionary[1].Constraints.Add(DOFType.RotY);
-            model.NodesDictionary[1].Constraints.Add(DOFType.RotZ);  
+            model.NodesDictionary[1].Constraints.Add(DOFType.RotZ);
             // Generate elements of the structure
             int iNode = 0;
             for (int iElem = 0; iElem < nElems; iElem++)
@@ -124,26 +96,25 @@ namespace ISAAR.MSolve.Tests
                 //};
 
                 // Create new Beam3D section and element
-                var beamSection = new BeamSection3D(area, inertiaY, inertiaZ, torsionalInertia, effectiveAreaY, effectiveAreaZ);
-                var beam = new Beam3DCorotationalQuaternion(nodes, material, 7.85, beamSection);
+                var hexa = new Hexa8NL_v2();
 
                 // Create elements
                 var element = new Element()
                 {
                     ID = iElem + 1,
-                    ElementType = beam
+                    ElementType = hexa
                 };
                 // Add nodes to the created element
                 element.AddNode(model.NodesDictionary[iNode]);
                 element.AddNode(model.NodesDictionary[iNode + 1]);
 
-                var a = beam.StiffnessMatrix(element);
+                var a = hexa.StiffnessMatrix(element);
 
                 // Add Hexa element to the element and subdomains dictionary of the model
                 model.ElementsDictionary.Add(element.ID, element);
                 model.SubdomainsDictionary[1].ElementsDictionary.Add(element.ID, element);
                 iNode++;
-            }    
+            }
 
             // Add nodal load values at the top nodes of the model
             model.Loads.Add(new Load() { Amount = nodalLoad, Node = model.NodesDictionary[monitorNode], DOF = DOFType.Y });
