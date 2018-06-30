f307fd45a7e81afb52e0d0b3acabb7a54d98870a
Dimitris Tsapetis <dimtsap@hotmail.com>
2018-06-30 16:33:39 +0300

    Creates GaussLegendre3D class

--- ISAAR.MSolve.Discretization/Integration/Quadratures/IQuadrature3D.cs
+++ ISAAR.MSolve.Discretization/Integration/Quadratures/IQuadrature3D.cs
@@ -15,6 +15,6 @@ namespace ISAAR.MSolve.Discretization.Integration.Quadratures
 		/// <summary>
 		/// The integration points are sorted based on an order strictly defined for each quadrature.
 		/// </summary>
-		IReadOnlyList<GaussPoint3D> Integrationpoints { get; }
+		IReadOnlyList<GaussPoint3D> IntegrationPoints { get; }
 	}
 }
