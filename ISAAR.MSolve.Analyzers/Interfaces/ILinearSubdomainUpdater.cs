83989ff1368aeab711f7f106484d430b2856858b
Maria Tavlaki <mariataylaki@gmail.com>
2018-07-01 18:29:36 +0300

    Add interface ILinearSubdomainUpdater.

---
+++ ISAAR.MSolve.Analyzers/Interfaces/ILinearSubdomainUpdater.cs
@@ -0,0 +1,12 @@
+﻿using ISAAR.MSolve.Numerical.LinearAlgebra.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ISAAR.MSolve.Analyzers.Interfaces
+{
+    public interface ILinearSubdomainUpdater
+    {
+        IVector GetRHS();
+    }
+}
