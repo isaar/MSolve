ab8ff30605c1a8f22eaf7e95177f2797c9bb3c92
Dimitris Tsapetis <dimtsap@hotmail.com>
2018-07-02 21:51:09 +0300

    Creates Node3D class

--- ISAAR.MSolve.Discretization/Providers/ElementStructuralDampingProvider.cs
+++ ISAAR.MSolve.Discretization/Providers/ElementStructuralDampingProvider.cs
@@ -2,7 +2,7 @@
 using ISAAR.MSolve.FEM.Interfaces;
 using ISAAR.MSolve.Numerical.LinearAlgebra.Interfaces;
 
-namespace ISAAR.MSolve.FEM.Providers
+namespace ISAAR.MSolve.Discretization.Providers
 {
     public class ElementStructuralDampingProvider : IElementMatrixProvider
     {
