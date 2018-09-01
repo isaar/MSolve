57b513fea29f00270116fd57b4fb6f2ea88e6464
Dimitris Tsapetis <dimtsap@hotmail.com>
2018-09-01 12:27:33 +0300

    Verifies Hexa27 shape function derivatives

--- ISAAR.MSolve.FEM/Interpolation/InterpolationHexa27.cs
+++ ISAAR.MSolve.FEM/Interpolation/InterpolationHexa27.cs
@@ -100,16 +100,7 @@ namespace ISAAR.MSolve.FEM.Interpolation
 			values[8] = 1 / 4.0 * (1 - x * x) * y * (y - 1) * z * (z - 1);
 			values[9] = 1 / 4.0 * x * (x - 1) * (1 - y * y) * z * (z - 1);
 			values[10] = 1 / 4.0 * x * (x - 1) * y * (y - 1) * (1 - z * z);
-
-
-
-
 			values[11] = 1 / 4.0 * x * (x + 1) * (1 - y * y) * z * (z - 1);
-
-
-
-
-
 
 			values[12] = 1 / 4.0 * x * (x + 1) * y * (y - 1) * (1 - z * z);
 			values[13] = 1 / 4.0 * (1 - x * x) * y * (y + 1) * z * (z - 1);
@@ -149,34 +140,24 @@ namespace ISAAR.MSolve.FEM.Interpolation
 			derivatives[6, 0] = (x * y * z * (y + 1) * (z + 1)) / 8 + (y * z * (x + 1) * (y + 1) * (z + 1)) / 8;
 			derivatives[7, 0] = (x * y * z * (y + 1) * (z + 1)) / 8 + (y * z * (x - 1) * (y + 1) * (z + 1)) / 8;
 			derivatives[8, 0] = -(x * y * z * (y - 1) * (z - 1)) / 2;
-			derivatives[9, 0] = -(x * z * (Math.Pow(y, 2) - 1) * (z - 1)) / 4 -
-			                    (z * (Math.Pow(y, 2) - 1) * (x - 1) * (z - 1)) / 4;
-			derivatives[10, 0] = -(x * y * (Math.Pow(z, 2) - 1) * (y - 1)) / 4 -
-			                     (y * (Math.Pow(z, 2) - 1) * (x - 1) * (y - 1)) / 4;
-			derivatives[11, 0] = -(y * z * (Math.Pow(y, 2) - 1) * (x + 1) * (z - 1)) / 4 -
-			                     (x * y * z * (Math.Pow(y, 2) - 1) * (z - 1)) / 4;
-			derivatives[12, 0] = -(x * y * (Math.Pow(z, 2) - 1) * (y - 1)) / 4 -
-			                     (y * (Math.Pow(z, 2) - 1) * (x + 1) * (y - 1)) / 4;
+			derivatives[9, 0] = -(x * z * (y * y - 1) * (z - 1)) / 4 - (z * (y * y - 1) * (x - 1) * (z - 1)) / 4;
+			derivatives[10, 0] = -(x * y * (z * z - 1) * (y - 1)) / 4 - (y * (z * z - 1) * (x - 1) * (y - 1)) / 4;
+			derivatives[11, 0] = -(x * z * (y * y - 1) * (z - 1)) / 4 - (z * (y * y - 1) * (x + 1) * (z - 1)) / 4;
+			derivatives[12, 0] = -(x * y * (z * z - 1) * (y - 1)) / 4 - (y * (z * z - 1) * (x + 1) * (y - 1)) / 4;
 			derivatives[13, 0] = -(x * y * z * (y + 1) * (z - 1)) / 2;
-			derivatives[14, 0] = -(x * y * (Math.Pow(z, 2) - 1) * (y + 1)) / 4 -
-			                     (y * (Math.Pow(z, 2) - 1) * (x + 1) * (y + 1)) / 4;
-			derivatives[15, 0] = -(x * y * (Math.Pow(z, 2) - 1) * (y + 1)) / 4 -
-			                     (y * (Math.Pow(z, 2) - 1) * (x - 1) * (y + 1)) / 4;
+			derivatives[14, 0] = -(x * y * (z * z - 1) * (y + 1)) / 4 - (y * (z * z - 1) * (x + 1) * (y + 1)) / 4;
+			derivatives[15, 0] = -(x * y * (z * z - 1) * (y + 1)) / 4 - (y * (z * z - 1) * (x - 1) * (y + 1)) / 4;
 			derivatives[16, 0] = -(x * y * z * (y - 1) * (z + 1)) / 2;
-			derivatives[17, 0] = -(x * z * (Math.Pow(y, 2) - 1) * (z + 1)) / 4 -
-			                     (z * (Math.Pow(y, 2) - 1) * (x - 1) * (z + 1)) / 4;
-			derivatives[18, 0] = -(x * z * (Math.Pow(y, 2) - 1) * (z + 1)) / 4 -
-			                     (z * (Math.Pow(y, 2) - 1) * (x + 1) * (z + 1)) / 4;
+			derivatives[17, 0] = -(x * z * (y * y - 1) * (z + 1)) / 4 - (z * (y * y - 1) * (x - 1) * (z + 1)) / 4;
+			derivatives[18, 0] = -(x * z * (y * y - 1) * (z + 1)) / 4 - (z * (y * y - 1) * (x + 1) * (z + 1)) / 4;
 			derivatives[19, 0] = -(x * y * z * (y + 1) * (z + 1)) / 2;
-			derivatives[20, 0] = x * z * (Math.Pow(y, 2) - 1) * (z - 1);
-			derivatives[21, 0] = x * y * (Math.Pow(z, 2) - 1) * (y - 1);
-			derivatives[22, 0] = (x * (Math.Pow(y, 2) - 1) * (Math.Pow(z, 2) - 1)) / 2 +
-			                     ((Math.Pow(y, 2) - 1) * (Math.Pow(z, 2) - 1) * (x + 1)) / 2;
-			derivatives[23, 0] = (x * (Math.Pow(y, 2) - 1) * (Math.Pow(z, 2) - 1)) / 2 +
-			                     ((Math.Pow(y, 2) - 1) * (Math.Pow(z, 2) - 1) * (x + 1)) / 2;
-			derivatives[24, 0] = -x * (Math.Pow(y, 2) - 1) * (Math.Pow(z, 2) - 1);
-			derivatives[25, 0] = x * z * (Math.Pow(y, 2) - 1) * (z - 1);
-			derivatives[26, 0] = -x * (Math.Pow(y, 2) - 1) * (Math.Pow(z, 2) - 1);
+			derivatives[20, 0] = x * z * (y * y - 1) * (z - 1);
+			derivatives[21, 0] = x * y * (z * z - 1) * (y - 1);
+			derivatives[22, 0] = (x * (y * y - 1) * (z * z - 1)) / 2 + ((y * y - 1) * (z * z - 1) * (x - 1)) / 2;
+			derivatives[23, 0] = (x * (y * y - 1) * (z * z - 1)) / 2 + ((y * y - 1) * (z * z - 1) * (x + 1)) / 2;
+			derivatives[24, 0] = x * y * (z * z - 1) * (y + 1);
+			derivatives[25, 0] = x * z * (y * y - 1) * (z + 1);
+			derivatives[26, 0] = -2 * x * (y * y - 1) * (z * z - 1);
 
 			derivatives[0, 1] = (x * y * z * (x - 1) * (z - 1)) / 8 + (x * z * (x - 1) * (y - 1) * (z - 1)) / 8;
 			derivatives[1, 1] = (x * y * z * (x + 1) * (z - 1)) / 8 + (x * z * (x + 1) * (y - 1) * (z - 1)) / 8;
@@ -186,35 +167,25 @@ namespace ISAAR.MSolve.FEM.Interpolation
 			derivatives[5, 1] = (x * y * z * (x + 1) * (z + 1)) / 8 + (x * z * (x + 1) * (y - 1) * (z + 1)) / 8;
 			derivatives[6, 1] = (x * y * z * (x + 1) * (z + 1)) / 8 + (x * z * (x + 1) * (y + 1) * (z + 1)) / 8;
 			derivatives[7, 1] = (x * y * z * (x - 1) * (z + 1)) / 8 + (x * z * (x - 1) * (y + 1) * (z + 1)) / 8;
-			derivatives[8, 1] = -z * (Math.Pow(x, 2) / 4 - 1 / 4.0) * (y - 1) * (z - 1) -
-			                    y * z * (Math.Pow(x, 2) / 4 - 1 / 4.0) * (z - 1);
+			derivatives[8, 1] = -z * (x * x - 1) / 4 * (y - 1) * (z - 1) - y * z * (x * x - 1) / 4 * (z - 1);
 			derivatives[9, 1] = -(x * y * z * (x - 1) * (z - 1)) / 2;
-			derivatives[10, 1] = -(x * y * (Math.Pow(z, 2) - 1) * (x - 1)) / 4 -
-			                     (x * (Math.Pow(z, 2) - 1) * (x - 1) * (y - 1)) / 4;
-			derivatives[11, 1] = -(x * z * (Math.Pow(y, 2) - 1) * (x + 1) * (z - 1)) / 4 -
-			                     (x * Math.Pow(y, 2) * z * (x + 1) * (z - 1)) / 2;
-			derivatives[12, 1] = -(x * y * (Math.Pow(z, 2) - 1) * (x + 1)) / 4 -
-			                     (x * (Math.Pow(z, 2) - 1) * (x + 1) * (y - 1)) / 4;
-			derivatives[13, 1] = -z * (Math.Pow(x, 2) / 4 - 1 / 4.0) * (y + 1) * (z - 1) -
-			                     y * z * (Math.Pow(x, 2) / 4 - 1 / 4.0) * (z - 1);
-			derivatives[14, 1] = -(x * y * (Math.Pow(z, 2) - 1) * (x + 1)) / 4 -
-			                     (x * (Math.Pow(z, 2) - 1) * (x + 1) * (y + 1)) / 4;
-			derivatives[15, 1] = -(x * y * (Math.Pow(z, 2) - 1) * (x - 1)) / 4 -
-			                     (x * (Math.Pow(z, 2) - 1) * (x - 1) * (y + 1)) / 4;
-			derivatives[16, 1] = -z * (Math.Pow(x, 2) / 4 - 1 / 4.0) * (y - 1) * (z + 1) -
-			                     y * z * (Math.Pow(x, 2) / 4 - 1 / 4.0) * (z + 1);
+			derivatives[10, 1] = -(x * y * (z * z - 1) * (x - 1)) / 4 - (x * (z * z - 1) * (x - 1) * (y - 1)) / 4;
+			derivatives[11, 1] = -(x * y * z * (x + 1) * (z - 1)) / 2;
+			derivatives[12, 1] = -(x * y * (z * z - 1) * (x + 1)) / 4 - (x * (z * z - 1) * (x + 1) * (y - 1)) / 4;
+			derivatives[13, 1] = -z * (x * x - 1) / 4 * (y + 1) * (z - 1) - y * z * (x * x - 1) / 4 * (z - 1);
+			derivatives[14, 1] = -(x * y * (z * z - 1) * (x + 1)) / 4 - (x * (z * z - 1) * (x + 1) * (y + 1)) / 4;
+			derivatives[15, 1] = -(x * y * (z * z - 1) * (x - 1)) / 4 - (x * (z * z - 1) * (x - 1) * (y + 1)) / 4;
+			derivatives[16, 1] = -z * (x * x - 1) / 4 * (y - 1) * (z + 1) - y * z * (x * x - 1) / 4 * (z + 1);
 			derivatives[17, 1] = -(x * y * z * (x - 1) * (z + 1)) / 2;
 			derivatives[18, 1] = -(x * y * z * (x + 1) * (z + 1)) / 2;
-			derivatives[19, 1] = -z * (Math.Pow(x, 2) / 4 - 1 / 4.0) * (y + 1) * (z + 1) -
-			                     y * z * (Math.Pow(x, 2) / 4 - 1 / 4.0) * (z + 1);
-			derivatives[20, 1] = 2 * y * z * (Math.Pow(x, 2) / 2 - 1 / 2.0) * (z - 1);
-			derivatives[21, 1] = (Math.Pow(z, 2) - 1) * (Math.Pow(x, 2) / 2 - 1 / 2.0) * (y - 1) +
-			                     y * (Math.Pow(z, 2) - 1) * (Math.Pow(x, 2) / 2 - 1 / 2.0);
-			derivatives[22, 1] = x * y * (Math.Pow(z, 2) - 1) * (x + 1);
-			derivatives[23, 1] = x * y * (Math.Pow(z, 2) - 1) * (x + 1);
-			derivatives[24, 1] = -2 * y * (Math.Pow(z, 2) - 1) * (Math.Pow(x, 2) / 2 - 1 / 2.0);
-			derivatives[25, 1] = 2 * y * z * (Math.Pow(x, 2) / 2 - 1 / 2.0) * (z - 1);
-			derivatives[26, 1] = -2 * y * (Math.Pow(z, 2) - 1) * (Math.Pow(x, 2) / 2 - 1 / 2.0);
+			derivatives[19, 1] = -z * (x * x - 1) / 4 * (y + 1) * (z + 1) - y * z * (x * x - 1) / 4 * (z + 1);
+			derivatives[20, 1] = 2 * y * z * (x * x - 1) / 2 * (z - 1);
+			derivatives[21, 1] = (z * z - 1) * (x * x - 1) / 2 * (y - 1) + y * (z * z - 1) * (x * x - 1) / 2;
+			derivatives[22, 1] = x * y * (z * z - 1) * (x - 1);
+			derivatives[23, 1] = x * y * (z * z - 1) * (x + 1);
+			derivatives[24, 1] = (z * z - 1) * (x * x - 1) / 2 * (y + 1) + y * (z * z - 1) * (x * x - 1) / 2;
+			derivatives[25, 1] = 2 * y * z * (x * x - 1) / 2 * (z + 1);
+			derivatives[26, 1] = -2 * y * (x * x - 1) * (z * z - 1);
 
 			derivatives[0, 2] = (x * y * z * (x - 1) * (y - 1)) / 8 + (x * y * (x - 1) * (y - 1) * (z - 1)) / 8;
 			derivatives[1, 2] = (x * y * z * (x + 1) * (y - 1)) / 8 + (x * y * (x + 1) * (y - 1) * (z - 1)) / 8;
@@ -224,35 +195,25 @@ namespace ISAAR.MSolve.FEM.Interpolation
 			derivatives[5, 2] = (x * y * z * (x + 1) * (y - 1)) / 8 + (x * y * (x + 1) * (y - 1) * (z + 1)) / 8;
 			derivatives[6, 2] = (x * y * z * (x + 1) * (y + 1)) / 8 + (x * y * (x + 1) * (y + 1) * (z + 1)) / 8;
 			derivatives[7, 2] = (x * y * z * (x - 1) * (y + 1)) / 8 + (x * y * (x - 1) * (y + 1) * (z + 1)) / 8;
-			derivatives[8, 2] = -y * (Math.Pow(x, 2) / 4 - 1 / 4.0) * (y - 1) * (z - 1) -
-			                    y * z * (Math.Pow(x, 2) / 4 - 1 / 4.0) * (y - 1);
-			derivatives[9, 2] = -(x * z * (Math.Pow(y, 2) - 1) * (x - 1)) / 4 -
-			                    (x * (Math.Pow(y, 2) - 1) * (x - 1) * (z - 1)) / 4;
+			derivatives[8, 2] = -y * (x * x - 1) / 4 * (y - 1) * (z - 1) - y * z * (x * x - 1) / 4 * (y - 1);
+			derivatives[9, 2] = -(x * z * (y * y - 1) * (x - 1)) / 4 - (x * (y * y - 1) * (x - 1) * (z - 1)) / 4;
 			derivatives[10, 2] = -(x * y * z * (x - 1) * (y - 1)) / 2;
-			derivatives[11, 2] = -(x * y * (Math.Pow(y, 2) - 1) * (x + 1) * (z - 1)) / 4 -
-			                     (x * y * z * (Math.Pow(y, 2) - 1) * (x + 1)) / 4;
+			derivatives[11, 2] = -(x * z * (y * y - 1) * (x + 1)) / 4 - (x * (y * y - 1) * (x + 1) * (z - 1)) / 4;
 			derivatives[12, 2] = -(x * y * z * (x + 1) * (y - 1)) / 2;
-			derivatives[13, 2] = -y * (Math.Pow(x, 2) / 4 - 1 / 4.0) * (y + 1) * (z - 1) -
-			                     y * z * (Math.Pow(x, 2) / 4 - 1 / 4.0) * (y + 1);
+			derivatives[13, 2] = -y * (x * x - 1) / 4 * (y + 1) * (z - 1) - y * z * (x * x - 1) / 4 * (y + 1);
 			derivatives[14, 2] = -(x * y * z * (x + 1) * (y + 1)) / 2;
 			derivatives[15, 2] = -(x * y * z * (x - 1) * (y + 1)) / 2;
-			derivatives[16, 2] = -y * (Math.Pow(x, 2) / 4 - 1 / 4.0) * (y - 1) * (z + 1) -
-			                     y * z * (Math.Pow(x, 2) / 4 - 1 / 4.0) * (y - 1);
-			derivatives[17, 2] = -(x * z * (Math.Pow(y, 2) - 1) * (x - 1)) / 4 -
-			                     (x * (Math.Pow(y, 2) - 1) * (x - 1) * (z + 1)) / 4;
-			derivatives[18, 2] = -(x * z * (Math.Pow(y, 2) - 1) * (x + 1)) / 4 -
-			                     (x * (Math.Pow(y, 2) - 1) * (x + 1) * (z + 1)) / 4;
-			derivatives[19, 2] = -y * (Math.Pow(x, 2) / 4 - 1 / 4.0) * (y + 1) * (z + 1) -
-			                     y * z * (Math.Pow(x, 2) / 4 - 1 / 4.0) * (y + 1);
-			derivatives[20, 2] = (Math.Pow(y, 2) - 1) * (Math.Pow(x, 2) / 2 - 1 / 2.0) * (z - 1) +
-			                     z * (Math.Pow(y, 2) - 1) * (Math.Pow(x, 2) / 2 - 1 / 2.0);
-			derivatives[21, 2] = 2 * y * z * (Math.Pow(x, 2) / 2 - 1 / 2.0) * (y - 1);
-			derivatives[22, 2] = x * z * (Math.Pow(y, 2) - 1) * (x + 1);
-			derivatives[23, 2] = x * z * (Math.Pow(y, 2) - 1) * (x + 1);
-			derivatives[24, 2] = -2 * z * (Math.Pow(y, 2) - 1) * (Math.Pow(x, 2) / 2 - 1 / 2.0);
-			derivatives[25, 2] = (Math.Pow(y, 2) - 1) * (Math.Pow(x, 2) / 2 - 1 / 2.0) * (z - 1) +
-			                     z * (Math.Pow(y, 2) - 1) * (Math.Pow(x, 2) / 2 - 1 / 2.0);
-			derivatives[26, 2] = -2 * z * (Math.Pow(y, 2) - 1) * (Math.Pow(x, 2) / 2 - 1 / 2.0);
+			derivatives[16, 2] = -y * (x * x - 1) / 4 * (y - 1) * (z + 1) - y * z * (x * x - 1) / 4 * (y - 1);
+			derivatives[17, 2] = -(x * z * (y * y - 1) * (x - 1)) / 4 - (x * (y * y - 1) * (x - 1) * (z + 1)) / 4;
+			derivatives[18, 2] = -(x * z * (y * y - 1) * (x + 1)) / 4 - (x * (y * y - 1) * (x + 1) * (z + 1)) / 4;
+			derivatives[19, 2] = -y * (x * x - 1) / 4 * (y + 1) * (z + 1) - y * z * (x * x - 1) / 4 * (y + 1);
+			derivatives[20, 2] = (y * y - 1) * (x * x - 1) / 2 * (z - 1) + z * (y * y - 1) * (x * x - 1) / 2;
+			derivatives[21, 2] = 2 * y * z * (x * x - 1) / 2 * (y - 1);
+			derivatives[22, 2] = x * z * (y * y - 1) * (x - 1);
+			derivatives[23, 2] = x * z * (y * y - 1) * (x + 1);
+			derivatives[24, 2] = (z * z - 1) * (x * x - 1) / 2 * (y + 1) + y * (z * z - 1) * (x * x - 1) / 2;
+			derivatives[25, 2] = (y * y - 1) * (x * x - 1) / 2 * (z + 1) + z * (y * y - 1) * (x * x - 1) / 2;
+			derivatives[26, 2] = -2 * z * (x * x - 1) * (y * y - 1);
 
 			return derivatives;
 		}
