b8be1541d37c1b983434d8201e5973c02be345d6
Maria Tavlaki <mariataylaki@gmail.com>
2018-09-19 19:19:26 +0300

    Move method CalculateEquivalentNodalLoads of EquivalentLoadsAssembler to INonLinearSubdomainUpdater.

--- ISAAR.MSolve.Analyzers/Interfaces/INonLinearSubdomainUpdater.cs
+++ ISAAR.MSolve.Analyzers/Interfaces/INonLinearSubdomainUpdater.cs
@@ -7,6 +7,7 @@ namespace ISAAR.MSolve.Analyzers.Interfaces
     public interface INonLinearSubdomainUpdater
     {
         void ScaleConstraints(double scalingFactor);
+        IVector CalculateEquivalentNodalLoads(IElementMatrixProvider elementProvider, IVector solution, IVector dSolution);
         IVector GetRHSFromSolution(IVector solution, IVector dSolution);
         void UpdateState();
         void ResetState();
--- ISAAR.MSolve.Analyzers/NonLinearSubdomainUpdater.cs
+++ ISAAR.MSolve.Analyzers/NonLinearSubdomainUpdater.cs
@@ -24,6 +24,35 @@ namespace ISAAR.MSolve.Analyzers
             this.subdomain.ScaleConstraints(scalingFactor);
         }
 
+        public IVector CalculateEquivalentNodalLoads(IElementMatrixProvider elementProvider, IVector solution, IVector dSolution) //TODOMaria this should also take as argument the nodal displacements of the constraints (after the refactoring)
+        {
+            var times = new Dictionary<string, TimeSpan>();
+            var totalStart = DateTime.Now;
+            times.Add("rowIndexCalculation", DateTime.Now - totalStart);
+            times.Add("element", TimeSpan.Zero);
+            times.Add("addition", TimeSpan.Zero);
+            var subdomainEquivalentNodalForces = new double[this.subdomain.Forces.Length];
+            foreach (Element element in subdomain.ElementsDictionary.Values)
+            {
+                var isEmbeddedElement = element.ElementType is IEmbeddedElement;
+                var elStart = DateTime.Now;
+                IMatrix2D ElementK = elementProvider.Matrix(element);
+
+                double[] localSolution = subdomain.CalculateElementNodalDisplacements(element, solution);
+                double[] localdSolution = subdomain.CalculateElementNodalDisplacements(element, dSolution);
+
+                var equivalentNodalForces = new double[localSolution.Length];
+                ElementK.Multiply(new Vector(localSolution), equivalentNodalForces);
+                subdomain.AddLocalVectorToGlobal(element, equivalentNodalForces, subdomainEquivalentNodalForces);
+
+                times["addition"] += DateTime.Now - elStart;
+            }
+
+            var totalTime = DateTime.Now - totalStart;
+
+            return new Vector(subdomainEquivalentNodalForces);
+        }
+
         public IVector GetRHSFromSolution(IVector solution, IVector dSolution) //TODO leave 
         {
             return this.subdomain.GetRHSFromSolution(solution, dSolution);
