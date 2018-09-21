b3bce4c43490592b77104f50e7d712f295b5238c
Gerasimos01 <acivil@hotmail.gr>
2018-09-21 19:50:07 +0300

    Added cohesive zone material.

---
+++ ISAAR.MSolve.Materials/Interfaces/ICohesiveZoneMaterial3D.cs
@@ -0,0 +1,20 @@
+﻿
+using ISAAR.MSolve.Numerical.LinearAlgebra;
+using ISAAR.MSolve.Numerical.LinearAlgebra.Interfaces;
+
+namespace ISAAR.MSolve.Materials.Interfaces
+{
+    public interface ICohesiveZoneMaterial3D  
+    {
+        double[] Tractions { get; }
+        IMatrix2D ConstitutiveMatrix { get; }
+        void UpdateMaterial(double[] strains);
+        int ID { get; }
+        bool Modified { get; }
+        void ResetModified();
+        void SaveState();
+        void ClearState();
+        void ClearTractions();
+        ICohesiveZoneMaterial3D Clone();
+    }
+}
--- ISAAR.MSolve.Materials/Interfaces/IFiniteElementMaterial.cs
+++ ISAAR.MSolve.Materials/Interfaces/IFiniteElementMaterial.cs
@@ -7,6 +7,7 @@ namespace ISAAR.MSolve.Materials.Interfaces
 {
     public interface IFiniteElementMaterial: ICloneable
     {
+        //this is only for Structural elements with strain and stress tensors.
         int ID { get; }
         bool Modified { get; }
         void ResetModified();
