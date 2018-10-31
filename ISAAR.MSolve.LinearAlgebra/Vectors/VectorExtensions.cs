d857ba170e62b9011c5d088308e4f8d2c0460d99
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2018-10-31 02:07:03 +0200

    Added extension methods Add, AddIntoThis, Subtract, SubtractIntoThis for the vector interfaces.
    Signed-off-by: Serafeim Bakalakos <serafeim.bakalakos@outlook.com>

--- ISAAR.MSolve.LinearAlgebra/Vectors/VectorExtensions.cs
+++ ISAAR.MSolve.LinearAlgebra/Vectors/VectorExtensions.cs
@@ -8,30 +8,125 @@ namespace ISAAR.MSolve.LinearAlgebra.Vectors
     public static class VectorExtensions
     {
         /// <summary>
-        /// Performs the operation: result[i] = <paramref name="vector1"/>[i] + <paramref name="vector2"/>[i], 
-        /// for 0 &lt;= i &lt; <paramref name="vector1"/>.<see cref="Length"/> = <paramref name="vector2"/>.<see cref="Length"/>.
-        /// The resulting vector overwrites the entries of this <see cref="Vector"/> instance.
+        /// Performs the operation: 
+        /// result[i] = <paramref name="thisVector"/>[i] + <paramref name="otherVector"/>[i], for 
+        /// 0 &lt;= i &lt; <paramref name="thisVector"/>.<see cref="IIndexable1D.Length"/> 
+        /// = <paramref name="otherVector"/>.<see cref="IIndexable1D.Length"/>.
+        /// The resulting vector is written in a new object and then returned.
         /// </summary>
-        /// <param name="vector1">The first <see cref="Vector"/> operand. It must have the same <see cref="Length"/> as 
-        ///     <paramref name="vector2"/>.</param>
-        /// <param name="vector2">The second <see cref="Vector"/> operand. It must have the same <see cref="Length"/> as 
-        ///     <paramref name="vector1"/>.</param>
-        /// <exception cref="NonMatchingDimensionsException">Thrown if <paramref name="vector1"/> and <paramref name="vector2"/>
-        ///     have different <see cref="Length"/>.</exception>
-        public static void AddIntoThis(this Vector vector1, Vector vector2) => vector1.AxpyIntoThis(vector2, 1.0);
+        /// <param name="thisVector">
+        /// A vector with the same <see cref="IIndexable1D.Length"/> as <paramref name="otherVector"/>.
+        /// </param>
+        /// <param name="otherVector">
+        /// A vector with the same <see cref="IIndexable1D.Length"/> as <paramref name="thisVector"/>.
+        /// </param>
+        /// <exception cref="NonMatchingDimensionsException">
+        /// Thrown if <paramref name="thisVector"/> and <paramref name="otherVector"/> have different 
+        /// <see cref="IIndexable1D.Length"/>.
+        /// </exception>
+        public static IVectorView Add(this IVectorView thisVector, IVectorView otherVector)
+            => thisVector.Axpy(otherVector, 1.0);
 
         /// <summary>
-        /// Performs the operation: result[i] = <paramref name="vector1"/>[i] - <paramref name="vector2"/>[i], 
-        /// for 0 &lt;= i &lt; <paramref name="vector1"/>.<see cref="Length"/> = <paramref name="vector2"/>.<see cref="Length"/>.
+        /// Performs the operation: 
+        /// <paramref name="thisVector"/>[i] = <paramref name="thisVector"/>[i] + <paramref name="otherVector"/>[i], for 
+        /// 0 &lt;= i &lt; <paramref name="thisVector"/>.<see cref="IIndexable1D.Length"/> 
+        /// = <paramref name="otherVector"/>.<see cref="IIndexable1D.Length"/>.
+        /// The resulting vector overwrites the entries of this <see cref="IVector"/> instance.
+        /// </summary>
+        /// <param name="thisVector">
+        /// A vector with the same <see cref="IIndexable1D.Length"/> as <paramref name="otherVector"/>.
+        /// </param>
+        /// <param name="otherVector">
+        /// A vector with the same <see cref="IIndexable1D.Length"/> as <paramref name="thisVector"/>.
+        /// </param>
+        /// <exception cref="NonMatchingDimensionsException">
+        /// Thrown if <paramref name="thisVector"/> and <paramref name="otherVector"/> have different 
+        /// <see cref="IIndexable1D.Length"/>.
+        /// </exception>
+        public static void AddIntoThis(this IVector thisVector, IVectorView otherVector) 
+            => thisVector.AxpyIntoThis(otherVector, 1.0);
+
+
+        /// <summary>
+        /// Performs the operation: 
+        /// <paramref name="thisVector"/>[i] = <paramref name="thisVector"/>[i] + <paramref name="otherVector"/>[i], for 
+        /// 0 &lt;= i &lt; <paramref name="thisVector"/>.<see cref="IIndexable1D.Length"/> 
+        /// = <paramref name="otherVector"/>.<see cref="IIndexable1D.Length"/>.
         /// The resulting vector overwrites the entries of this <see cref="Vector"/> instance.
         /// </summary>
-        /// <param name="vector1">The first <see cref="Vector"/> operand. It must have the same <see cref="Length"/> as 
-        ///     <paramref name="vector2"/>.</param>
-        /// <param name="vector2">The second <see cref="Vector"/> operand. It must have the same <see cref="Length"/> as 
-        ///     <paramref name="vector1"/>.</param>
-        /// <exception cref="NonMatchingDimensionsException">Thrown if <paramref name="vector1"/> and <paramref name="vector2"/>
-        ///     have different <see cref="Length"/>.</exception>
-        public static void SubtractIntoThis(this Vector vector1, Vector vector2) => vector1.AxpyIntoThis(vector2, -1.0);
+        /// <param name="thisVector">
+        /// A vector with the same <see cref="IIndexable1D.Length"/> as <paramref name="otherVector"/>.
+        /// </param>
+        /// <param name="otherVector">
+        /// A vector with the same <see cref="IIndexable1D.Length"/> as <paramref name="thisVector"/>.
+        /// </param>
+        /// <exception cref="NonMatchingDimensionsException">
+        /// Thrown if <paramref name="thisVector"/> and <paramref name="otherVector"/> have different 
+        /// <see cref="IIndexable1D.Length"/>.
+        /// </exception>
+        public static void AddIntoThis(this Vector thisVector, Vector otherVector) 
+            => thisVector.AxpyIntoThis(otherVector, 1.0);
+
+        /// <summary>
+        /// Performs the operation: 
+        /// result[i] = <paramref name="thisVector"/>[i] - <paramref name="otherVector"/>[i], for 
+        /// 0 &lt;= i &lt; <paramref name="thisVector"/>.<see cref="IIndexable1D.Length"/> 
+        /// = <paramref name="otherVector"/>.<see cref="IIndexable1D.Length"/>.
+        /// The resulting vector is written in a new object and then returned.
+        /// </summary>
+        /// <param name="thisVector">
+        /// A vector with the same <see cref="IIndexable1D.Length"/> as <paramref name="otherVector"/>.
+        /// </param>
+        /// <param name="otherVector">
+        /// A vector with the same <see cref="IIndexable1D.Length"/> as <paramref name="thisVector"/>.
+        /// </param>
+        /// <exception cref="NonMatchingDimensionsException">
+        /// Thrown if <paramref name="thisVector"/> and <paramref name="otherVector"/> have different 
+        /// <see cref="IIndexable1D.Length"/>.
+        /// </exception>
+        public static IVectorView Subtract(this IVectorView thisVector, IVectorView otherVector)
+            => thisVector.Axpy(otherVector, -1.0);
+
+        /// <summary>
+        /// Performs the operation: 
+        /// <paramref name="thisVector"/>[i] = <paramref name="thisVector"/>[i] - <paramref name="otherVector"/>[i], 
+        /// for 0 &lt;= i &lt; <paramref name="thisVector"/>.<see cref="IIndexable1D.Length"/> 
+        /// = <paramref name="otherVector"/>.<see cref="IIndexable1D.Length"/>.
+        /// The resulting vector overwrites the entries of this <see cref="IVector"/> instance.
+        /// </summary>
+        /// <param name="thisVector">
+        /// A vector with the same <see cref="IIndexable1D.Length"/> as <paramref name="otherVector"/>.
+        /// </param>
+        /// <param name="otherVector">
+        /// A vector with the same <see cref="IIndexable1D.Length"/> as <paramref name="thisVector"/>.
+        /// </param>
+        /// <exception cref="NonMatchingDimensionsException">
+        /// Thrown if <paramref name="thisVector"/> and <paramref name="otherVector"/> have different 
+        /// <see cref="IIndexable1D.Length"/>.
+        /// </exception>
+        public static void SubtractIntoThis(this IVector thisVector, IVectorView otherVector)
+            => thisVector.AxpyIntoThis(otherVector, -1.0);
+
+        /// <summary>
+        /// Performs the operation: 
+        /// <paramref name="thisVector"/>[i] = <paramref name="thisVector"/>[i] - <paramref name="otherVector"/>[i], 
+        /// for 0 &lt;= i &lt; <paramref name="thisVector"/>.<see cref="IIndexable1D.Length"/> 
+        /// = <paramref name="otherVector"/>.<see cref="IIndexable1D.Length"/>.
+        /// The resulting vector overwrites the entries of this <see cref="Vector"/> instance.
+        /// </summary>
+        /// <param name="thisVector">
+        /// A vector with the same <see cref="IIndexable1D.Length"/> as <paramref name="otherVector"/>.
+        /// </param>
+        /// <param name="otherVector">
+        /// A vector with the same <see cref="IIndexable1D.Length"/> as <paramref name="thisVector"/>.
+        /// </param>
+        /// <exception cref="NonMatchingDimensionsException">
+        /// Thrown if <paramref name="thisVector"/> and <paramref name="otherVector"/> have different 
+        /// <see cref="IIndexable1D.Length"/>.
+        /// </exception>
+        public static void SubtractIntoThis(this Vector thisVector, Vector otherVector) 
+            => thisVector.AxpyIntoThis(otherVector, -1.0);
 
         //TODO: remove this
         public static Numerical.LinearAlgebra.Vector ToLegacyVector(this IVectorView vector)
