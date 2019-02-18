fca1c74c91d52f0819cd95f36b7f9653eccfab67
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2019-02-18 11:34:25 +0200

    Added a DOF enumerator that scales the stiffness matrix and forces vector to simulate penalized material properties.
    Signed-off-by: Serafeim Bakalakos <serafeim.bakalakos@outlook.com>

---
+++ ISAAR.MSolve.Optimization.Structural/Topology/SIMP/PenaltyDofEnumerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ISAAR.MSolve.Discretization.Interfaces;
+using ISAAR.MSolve.LinearAlgebra;
+using ISAAR.MSolve.LinearAlgebra.Matrices;
+
+namespace ISAAR.MSolve.Optimization.Structural.Topology.SIMP
+{
+    public class PenaltyDofEnumerator : IElementDofEnumerator_v2
+    {
+        public double Penalty { get; set; } = double.NaN;
+
+        public IList<IList<DOFType>> GetDOFTypes(IElement_v2 element)
+                    => element.ElementType.GetElementDOFTypes(element);
+
+        public IList<IList<DOFType>> GetDOFTypesForDOFEnumeration(IElement_v2 element)
+            => element.ElementType.GetElementDOFTypes(element);
+
+        public IList<INode> GetNodesForMatrixAssembly(IElement_v2 element) => element.Nodes;
+
+        public IMatrix GetTransformedMatrix(IMatrix matrix) => matrix.Scale(Penalty); //TODO: it would be faster to not create a new object.
+
+        public double[] GetTransformedDisplacementsVector(double[] vector) => vector; //TODO: should this be scaled?
+
+        public double[] GetTransformedForcesVector(double[] vector) => vector.Scale(Penalty); //TODO: should this be scaled?
+
+    }
+}
