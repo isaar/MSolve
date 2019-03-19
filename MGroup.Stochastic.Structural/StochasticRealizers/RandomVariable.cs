73ce6e905e89d188bb1014c88fd4c3266350cbe7
Odysseas Kokkinos <odyred@gmail.com>
2019-03-19 12:25:54 +0200

    complemented class

--- MGroup.Stochastic.Structural/StochasticRealizers/RandomVariable.cs
+++ MGroup.Stochastic.Structural/StochasticRealizers/RandomVariable.cs
@@ -2,23 +2,52 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using ISAAR.MSolve.FEM.Interfaces;
+using ISAAR.MSolve.Materials.Interfaces;
+using PommaLabs.Thrower;
+using Troschuetz.Random;
+using Troschuetz.Random.Distributions.Continuous;
 
 namespace MGroup.Stochastic.Structural.StochasticRealizers
 {
+    public enum RandomVariableDistributionType
+    {
+        Normal,
+        Lognormal
+    }
     public class RandomVariable : IUncertainParameterRealizer
     {
-        private Random random = new Random();
-        public double Magnitude { get; }
+        private readonly double _standardDeviation;
+        private readonly double _meanValue;
+        private double _randomVariable;
+        private readonly RandomVariableDistributionType _distributionType;
         public IStochasticDomainMapper DomainMapper;
-        public RandomVariable(double magnitude, IStochasticDomainMapper domainMapper)
+
+        public RandomVariable(double meanValue, double standardDeviation, RandomVariableDistributionType distributionType)
         {
-            Magnitude = magnitude;
-            DomainMapper = domainMapper;
+            this._meanValue = meanValue;
+            this._standardDeviation = standardDeviation;
+            _distributionType = distributionType;
         }
 
         public double Realize(int iteration, IStochasticDomainMapper domainMapper, double[] parameters)
         {
-            return random.NextDouble() * Magnitude ;
+            switch (_distributionType)
+            {
+                case RandomVariableDistributionType.Normal:
+                    var normalDistribution = new NormalDistribution(_meanValue, _standardDeviation);
+                    return _randomVariable = normalDistribution.NextDouble();
+                    break;
+                case RandomVariableDistributionType.Lognormal:
+                    var lognormal = new LognormalDistribution(_meanValue, _standardDeviation * _meanValue);
+                    return _randomVariable = lognormal.NextDouble();
+                    break;
+                default:
+                    return 0;
+                    throw new NotImplementedException();
+                    break;
+            }
         }
+
     }
 }
