594e428d76c2d0c0f6c28adeefb76327a4743363
Dimitris Tsapetis <dimtsap@hotmail.com>
2019-04-02 15:02:18 +0300

    Adds Serafeim DokColMajor and DokRowMajor classes

--- ISAAR.MSolve.LinearAlgebra/Matrices/Builders/DokColMajor.cs
+++ ISAAR.MSolve.LinearAlgebra/Matrices/Builders/DokColMajor.cs
@@ -148,6 +148,36 @@ namespace ISAAR.MSolve.LinearAlgebra.Matrices.Builders
         }
 
         /// <summary>
+        /// See <see cref="IGeneralMatrixBuilder.AddSubmatrix(IIndexable2D, int[], int[], int[], int[])"/>.
+        /// </summary>
+        public void AddSubmatrix(IIndexable2D subMatrix, int[] subMatrixRows, int[] globalMatrixRows, 
+            int[] subMatrixCols, int[] globalMatrixCols)
+        {
+            int numRows = subMatrixRows.Length;
+            int numCols = subMatrixCols.Length;
+            Debug.Assert(numRows == globalMatrixRows.Length);
+            Debug.Assert(numCols == globalMatrixCols.Length);
+
+            for (int j = 0; j < numCols; ++j)
+            {
+                int subCol = subMatrixCols[j];
+                int globalCol = globalMatrixCols[j];
+                Debug.Assert((globalCol >= 0) && (globalCol < NumColumns));
+
+                for (int i = 0; i < numRows; ++i)
+                {
+                    int subRow = subMatrixRows[i];
+                    int globalRow = globalMatrixRows[i];
+                    Debug.Assert((globalRow >= 0) && (globalRow < NumRows));
+
+                    double subVal = subMatrix[subRow, subCol];
+                    columns[globalCol].TryGetValue(globalRow, out double oldGlobalVal); 
+                    columns[globalCol][globalRow] = subVal + oldGlobalVal; // default old value = 0.0, if the entry is new
+                }
+            }
+        }
+
+        /// <summary>
         /// See <see cref="IGeneralMatrixBuilder.AddSubmatrixSymmetric(IIndexable2D, IReadOnlyDictionary{int, int})"/>.
         /// </summary>
         public void AddSubmatrixSymmetric(IIndexable2D subMatrix, IReadOnlyDictionary<int, int> subIndicesToGlobalIndices)
