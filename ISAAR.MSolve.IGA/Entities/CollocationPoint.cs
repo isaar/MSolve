d0e1ce46f861531d75e853214754181626528bab
Dimitris Tsapetis <dimtsap@hotmail.com>
2019-04-04 13:22:51 +0300

    Adds Collocation Point2D

---
+++ ISAAR.MSolve.IGA/Entities/CollocationPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ISAAR.MSolve.Geometry.Coordinates;
+
+namespace ISAAR.MSolve.IGA.Entities
+{
+	public class CollocationPoint2D:NaturalPoint2D
+	{
+		public int ID { get; }
+
+		public CollocationPoint2D(int id, double xi, double eta) : base(xi, eta)
+		{
+			ID = id;
+		}
+
+		public CollocationPoint2D(int id, double[] coordinates) : base(coordinates)
+		{
+			ID = id;
+		}
+	}
+}
