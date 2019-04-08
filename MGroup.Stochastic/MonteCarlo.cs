ee8678edf5505dc2974131d6938d6cb74b2d0349
Odysseas Kokkinos <odyred@gmail.com>
2019-04-08 23:46:56 +0300

    created integration test, published statistical moments

--- MGroup.Stochastic/MonteCarlo.cs
+++ MGroup.Stochastic/MonteCarlo.cs
@@ -6,6 +6,8 @@ namespace MGroup.Stochastic
     public class MonteCarlo
     {
         public int NoOfIterations { get; }
+        public double[] MeanValueResponse;
+        public double[] StandardDeviationResponse;
         public ISystemRealizer SystemRealizer { get; }
         public ISystemResponseEvaluator SystemResponseEvaluator { get; }
 
@@ -26,12 +28,12 @@ namespace MGroup.Stochastic
             SystemRealizer.Realize(0);
             int systemResponseDimension = SystemResponseEvaluator.Evaluate(0).Length;
             var systemResponse = new double[systemResponseDimension];
-            var MonteCarloMeanValue = new double[systemResponseDimension];
-            var MonteCarloStandardDeviation = new double[systemResponseDimension];
+            MeanValueResponse = new double[systemResponseDimension];
+            StandardDeviationResponse = new double[systemResponseDimension];
             systemResponse = SystemResponseEvaluator.Evaluate(0);
             for (int i = 0; i < systemResponse.Length; i++)
             {
-                MonteCarloMeanValue[i] += systemResponse[i];
+                MeanValueResponse[i] += systemResponse[i];
             }
 
             for (int i = 1; i < NoOfIterations; i++)
@@ -40,15 +42,15 @@ namespace MGroup.Stochastic
                 systemResponse = SystemResponseEvaluator.Evaluate(i);
                 for (int j = 0; j < systemResponse.Length; j++)
                 {
-                    MonteCarloMeanValue[j] += systemResponse[j];
+                    MeanValueResponse[j] += systemResponse[j];
                 }
             }
 
             for (int i = 0; i < systemResponse.Length; i++)
             {
-                MonteCarloMeanValue[i] = MonteCarloMeanValue[i] / NoOfIterations;
-                MonteCarloStandardDeviation[i] = (systemResponse[i] - MonteCarloMeanValue[i]) * (systemResponse[i] - MonteCarloMeanValue[i]);
-                MonteCarloStandardDeviation[i] = Math.Sqrt(MonteCarloStandardDeviation[i] / NoOfIterations);
+                MeanValueResponse[i] = MeanValueResponse[i] / NoOfIterations;
+                StandardDeviationResponse[i] = (systemResponse[i] - MeanValueResponse[i]) * (systemResponse[i] - MeanValueResponse[i]);
+                StandardDeviationResponse[i] = Math.Sqrt(StandardDeviationResponse[i] / NoOfIterations);
             }
         }
     }
