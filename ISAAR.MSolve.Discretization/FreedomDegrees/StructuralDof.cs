c0dd76013cc7a96d24845af28bf9e352870d0b86
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2019-04-08 17:34:55 +0300

    Refactored the design of freedom degree types. Instead of a single enum, there is now a IDofType interface. The concrete classes follow the enum pattern. The reason for this is to accommodate XFEM.
    Signed-off-by: Serafeim Bakalakos <serafeim.bakalakos@outlook.com>

---
+++ ISAAR.MSolve.Discretization/FreedomDegrees/StructuralDof.cs
@@ -0,0 +1,47 @@
+﻿//TODO: implement a dynamic ordinal numbering that will change depending what dofs are used .
+namespace ISAAR.MSolve.Discretization.FreedomDegrees
+{
+    /// <summary>
+    /// Degrees of freedom corresponding to the movement of a point/body along or around the 3 possible axes. Implements enum 
+    /// pattern.
+    /// Authors: Serafeim Bakalakos
+    /// </summary>
+    public class StructuralDof: IDofType
+    {
+        /// <summary>
+        /// This dof corresponds to translation along X axis.
+        /// </summary>
+        public static readonly StructuralDof TranslationX = new StructuralDof("TranslationX.");
+
+        /// <summary>
+        /// This dof corresponds to translation along Y axis.
+        /// </summary>
+        public static readonly StructuralDof TranslationY = new StructuralDof("TranslationY");
+
+        /// <summary>
+        /// This dof corresponds to translation along Z axis.
+        /// </summary>
+        public static readonly StructuralDof TranslationZ = new StructuralDof("TranslationZ");
+
+        /// <summary>
+        /// This dof corresponds to rotation around X axis.
+        /// </summary>
+        public static readonly StructuralDof RotationX = new StructuralDof("RotationX.");
+
+        /// <summary>
+        /// This dof corresponds to rotation around Y axis.
+        /// </summary>
+        public static readonly StructuralDof RotationY = new StructuralDof("RotationY");
+
+        /// <summary>
+        /// This dof corresponds to rotation around Z axis.
+        /// </summary>
+        public static readonly StructuralDof RotationZ = new StructuralDof("RotationZ");
+
+        private readonly string name; 
+
+        private StructuralDof(string name) => this.name = name;
+
+        public override string ToString() => name;
+    }
+}
---
+++ ISAAR.MSolve.Discretization/FreedomDegrees/ThermalDof.cs
@@ -0,0 +1,20 @@
+﻿namespace ISAAR.MSolve.Discretization.FreedomDegrees
+{
+    /// <summary>
+    /// Degree of freedom corresponding to the temperature at a single point. Implements enum pattern.
+    /// Authors: Serafeim Bakalakos
+    /// </summary>
+    public class ThermalDof : IDofType
+    {
+        /// <summary>
+        /// This dof corresponds to the temperature.
+        /// </summary>
+        public static readonly ThermalDof Temperature = new ThermalDof("Temperature");
+
+        private readonly string name;
+
+        private ThermalDof(string name) => this.name = name;
+
+        public override string ToString() => name;
+    }
+}
